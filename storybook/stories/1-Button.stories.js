dfaacd53ba72bd7869a94ff1289c21b0fc0dce59
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-10-16 11:15:55 -0400

    Lerna and Storybook linked

--- storybook/stories/1-Button.stories.js
+++ storybook/stories/1-Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import Button  from '../components/Button';
+import Button  from 'design-system-react-components/Button';
 
 export default {
   title: 'Button',
