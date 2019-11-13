127f9011c8d1736f22142aca0dc5e47a653fd774
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-11-13 16:25:27 -0500

    [694] Legibility nits

--- src/twig/_patterns/02-molecules/menus/breadcrumbs/breadcrumbs.stories.js
+++ src/twig/_patterns/02-molecules/menus/breadcrumbs/breadcrumbs.stories.js
@@ -8,11 +8,17 @@ export default {
   // decorators: [storyFn => <div style={{ backgroundColor: "grey" }}>{storyFn()}</div>],
 };
 
-const shortItems = [{ url: "#", text: "Parent" }, { url: "#", text: "Home" }];
-const longItems = [{ url: "#", text: "Parent" },
-{ url: "#", text: "Home" },
-{ url: "#", text: "Roast Half And Half Pumpkin Spice Siphon Aroma Ristretto Cinnamon Saucer" },
-{ url: "#", text: "Roast Half And Half Pumpkin Spice Siphon Aroma Ristretto Cinnamon Saucer" }];
+const shortItems = [
+  { url: "#", text: "Parent" }, 
+  { url: "#", text: "Home" }
+];
+
+const longItems = [
+  { url: "#", text: "Parent" },
+  { url: "#", text: "Home" },
+  { url: "#", text: "Roast Half And Half Pumpkin Spice Siphon Aroma Ristretto Cinnamon Saucer" },
+  { url: "#", text: "Roast Half And Half Pumpkin Spice Siphon Aroma Ristretto Cinnamon Saucer" }
+];
 
 export const shortBreadcrumbs = () => Breadcrumbs({
   breadcrumbs: shortItems
