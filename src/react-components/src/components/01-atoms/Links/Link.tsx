2d12746e9093f3e5ab5edc38145444d919dbcb21
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-11-19 14:56:40 -0500

    Formatting and removed comment

--- src/react-components/src/components/01-atoms/Links/Link.tsx
+++ src/react-components/src/components/01-atoms/Links/Link.tsx
@@ -16,42 +16,13 @@ export default class Link extends React.Component<LinkProps, {}> {
   }
 
   render(): JSX.Element {
-  let link__base_class = "link";
-  const { url, attributes, className, modifiers, blockName, ...rest } = this.props;
-  let props = {
-    className: bem(link__base_class, modifiers, blockName),
-    href: url,
-  };
+    let link__base_class = "link";
+    const { url, attributes, className, modifiers, blockName, ...rest } = this.props;
+    let props = {
+      className: bem(link__base_class, modifiers, blockName),
+      href: url,
+    };
 
-  /*let children = React.Children.toArray(this.props.children);
-  console.log("children", children[0].valueOf().type);
-  if(children.length === 1 && children[0].type === "a") {
-    return React.createElement(
-      children[0].type,
-      {...props, ...children[0].props, ...rest},
-      children[0].props.children
-    );
-  } else {
-    // Do a bit of validation
-    if(!url) {
-      throw new Error("Please pass a 'url' into the Link");
-    }
-
-    let innerContent = children;
-    if(!innerContent) {
-      throw new Error("Please pass some visible content into the Link");
-    }
-    let childProps = children.map(child => child.props);
-    let elementChildren = children.map(child => {
-        return typeof(child) === "string" ? child : child.props.children; });
-
-    // Content/href/string was passed in.
-    return React.createElement(
-      "a",
-      {...props, ...childProps, ...rest},
-      elementChildren);
-    }
-    */
     let childProps = {};
     React.Children.forEach(this.props.children, (child: React.ReactElement, i: number) => {
       if (i === 0) {
@@ -61,12 +32,12 @@ export default class Link extends React.Component<LinkProps, {}> {
       }
     });
     let elementChildren = React.Children.map(this.props.children, (child: React.ReactElement) => {
-       return typeof (child) === "string" ? child : child.props.children;
-      });
+      return typeof (child) === "string" ? child : child.props.children;
+    });
     console.log("childProps", childProps);
     return React.createElement(
       "a",
-      { ...props, ...childProps, ...rest},
+      { ...props, ...childProps, ...rest },
       elementChildren
     );
 
