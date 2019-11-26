e281d5afb1a20c1bb2be851d1873548ca9900d82
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-11-26 15:50:35 -0500

    [638] Adds more-link modifiers to adjust icon position

--- src/react-components/src/components/01-atoms/Links/IconLink.tsx
+++ src/react-components/src/components/01-atoms/Links/IconLink.tsx
@@ -20,24 +20,21 @@ export default class IconLink extends React.Component<IconLinkProps, {}> {
     let iconLink__base_class = "more-link";
     const { url, attributes, modifiers, blockName, iconPosition } = this.props;
 
-    let className = bem(iconLink__base_class, modifiers, blockName);
-    let children = React.createElement(
-      "span",
-      { ...this.props, ...this.childProps },
-      this.props.children,
-    );
+    let moreLinkModifiers = modifiers ? modifiers : [];
+    let children = this.props.children;
 
     let icon: {element: JSX.Element, position: string} = {element: undefined, position: ""};
 
     if (iconPosition) {
-
       if (iconPosition === "left") {
+        moreLinkModifiers.push('icon-left');
         icon.element = <Icon
         name="arrow-xsmall"
         blockName={iconLink__base_class}
         modifiers={["left"]} decorative={true} />;
-        icon.position = "left";
+        icon.position = "icon-left";
       } else if (iconPosition === "right") {
+        moreLinkModifiers.push('icon-right');
         icon.element = <Icon
         name="arrow-xsmall"
         blockName={iconLink__base_class}
@@ -47,6 +44,9 @@ export default class IconLink extends React.Component<IconLinkProps, {}> {
         throw new Error("iconPosition can only have values \"left\" or \"right\"");
       }
     }
+
+    let className = bem(iconLink__base_class, moreLinkModifiers, blockName);
+
     return <Link url={url} className={className} attributes={attributes} children={children} icon={icon} />;
   }
 }
