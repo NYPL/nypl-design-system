ffb4fa370a5bf1358942293fbb301f6274348208
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-11-26 14:56:15 -0500

    exports

--- src/react-components/src/index.ts
+++ src/react-components/src/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./components/01-atoms/Button/Button";
+export { default as BodyText } from "./components/01-atoms/Text/BodyText";
 export { default as Breadcrumb } from "./components/02-molecules/menus/Breadcrumbs/Breadcrumbs";
