99b032ad795d10817aa99990ea58977c2449089d
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-12-03 09:27:51 -0500

    Added Placeholder

--- src/react-components/src/components/02-molecules/search-bar/SearchBar.stories.tsx
+++ src/react-components/src/components/02-molecules/search-bar/SearchBar.stories.tsx
@@ -9,3 +9,4 @@ export default {
 };
 
 export const basicSearch = () => <SearchBar id="searchbar" searchHandler={action("searched")}/>;
+export const searchWithPlaceholder = () => <SearchBar id="searchbar" placeholderText={"Placeholder"} searchHandler={action("searched")}/>;
