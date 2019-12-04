925766c77dd6b92c5f9f8ddbcd16e34440ced20b
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-12-04 10:47:45 -0500

    nit

--- src/react-components/src/__tests__/components/IconLinkList-test.tsx
+++ src/react-components/src/__tests__/components/IconLinkList-test.tsx
@@ -7,7 +7,6 @@ import * as Mocha from "mocha";
 import IconLinkList from "../../components/03-organisms/IconLinkList/IconLinkList";
 
 describe("IconLinkList", () => {
-  let wrapper: Enzyme.ShallowWrapper<{}, {}>;
   let linkContent = [{ text: "link1", url: "#url1" }, { text: "link2", url: "#url2" }];
   let linkContentComponent = [<a href="#url">link1</a>, <a href="#url">link1</a>];
   let notLinkContent = [<span>link1</span>, <span>link1</span>];
