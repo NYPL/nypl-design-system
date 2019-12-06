a4ead29a19c5cf4a8ac34970730e300a1285197d
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-12-06 17:01:42 -0500

    Updates the header-img-right css with the exception of green background extending beyond image

--- src/react-components/src/components/03-organisms/Headers/HeaderImgRight.tsx
+++ src/react-components/src/components/03-organisms/Headers/HeaderImgRight.tsx
@@ -21,16 +21,23 @@ export interface HeaderImgRightProps {
 
 export default function HeaderImgRight(props: React.PropsWithChildren<HeaderImgRightProps>) {
 
-  const { headerId, pageTitleText, isImageDecorative, imageAltText, imgUrl, imgAttributes, imgModifiers, bodyText} = props;
-  const baseClass = "section-header-img-right";
+    const { headerId, pageTitleText, imgUrl } = props;
+    const baseClass = "header-with-image-right";
 
-  let bodyTextContent = bodyText ? bodyText : props.children;
-
-  return (
-    <div className={bem(baseClass)}>
-      <PageTitle headerId={headerId} text={pageTitleText} /><Image isDecorative={isImageDecorative} altText={imageAltText} src={imgUrl} ></Image>
-      <BodyText warnchar={200}>{bodyTextContent}</BodyText>
-    </div>
-  );
-}
+    return (
+      <div className={bem(baseClass)}>
+        <div className={bem("content", [], baseClass)}>
+          <div className={bem("heading", [], baseClass)}>
+            <PageTitle headerId={headerId} text={pageTitleText}/>
+          </div>
+          <div className={bem("text", [], baseClass)}>
+            <BodyText bodyContent={<p>hello there lots of other text to see what bhappens liasjdfkljskj asldkjflksj asldkfjklji alsdkjkjk lajsldkjflkdj kjelkjlkj lkjlkj lkj  jlk lkj lj alejl lkjalskdjf sd fkasjdlfkjdkls laskdjfk dsjal asldkfjkldsja lkjasdklfj laskdjfdsj dljfldksj ljsldkfj kldsjkljalk sdjflk lasjdlkfjdlks ajasldkjf sklj skldjflkjs lskdfj dlksjlk</p>}></BodyText>
+          </div>
+        </div>
+        <div className={bem("image", [], baseClass)}>
+          <Image src={imgUrl} isDecorative={true}></Image>
+        </div>
+      </div>
+    );
+  }
 
