8056c9360c23a56424105217ebaa3d6d5e472d7e
Crystal Ngai <crystal.hk.ngai@gmail.com>
2019-12-17 09:53:26 -0500

    Changed up config to use new install location

--- storybook/.storybook-react/config.js
+++ storybook/.storybook-react/config.js
@@ -1,7 +1,7 @@
-import '!style-loader!css-loader!sass-loader!import-glob-loader!design-system-styles/style.scss';
+import '!style-loader!css-loader!sass-loader!import-glob-loader!@nypl/design-system-styles/style.scss';
 import { configure, addDecorator } from '@storybook/react';
 
 // automatically import all stories from node_modules/stories ending in *.stories.js
 // Direct link used instead of require(module) because of Webpack's inability to resolve variables in require.context
 
-configure(require.context('../../node_modules/design-system-react-components/src/components', true, /\.stories\.tsx$/), module);
+configure(require.context('../../node_modules/@nypl/design-system-react-components/src/components', true, /\.stories\.tsx$/), module);
