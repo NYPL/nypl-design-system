b768edb554b237542dc68c30a6de4286745bc251
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-06 16:42:22 +0000

    Deploy NYPL/nypl-design-system to github.com/NYPL/nypl-design-system.git:gh-pages

---
+++ storybook/storybook-static/react/precache-manifest.ddbb0a446d83c4b0a409a40c88df4c2b.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920ad0f00e07e0aa3a9c12025078b715",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.590dba17a1afbdd85ede.bundle.js"
+  },
+  {
+    "url": "runtime~main.590dba17a1afbdd85ede.bundle.js"
+  },
+  {
+    "revision": "30835763eeedc0d5032784f17b493a94",
+    "url": "static/media/sprite.30835763.svg"
+  },
+  {
+    "url": "vendors~main.590dba17a1afbdd85ede.bundle.js"
+  }
+]);
---
+++ storybook/storybook-static/twig/precache-manifest.8ce9249a945a555beeafdbd9a959aaed.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca4a0beb2fa985e68179dbaad647616",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.61664ebc8b94fb97999e.bundle.js"
+  },
+  {
+    "url": "runtime~main.61664ebc8b94fb97999e.bundle.js"
+  },
+  {
+    "url": "vendors~main.61664ebc8b94fb97999e.bundle.js"
+  }
+]);
