5526816c2d3c9a1392cbdc07bcd2cb100fb633c8
Crystal Ngai <crystal.hk.ngai@gmail.com>
2020-01-09 13:07:31 -0500

    re-added accidentally removed styles

--- src/react-components/src/components/01-atoms/Text/Headings/Heading.tsx
+++ src/react-components/src/components/01-atoms/Text/Headings/Heading.tsx
@@ -17,9 +17,11 @@ export interface HeadingProps {
 }
 
 export default function Heading(props: React.PropsWithChildren<HeadingProps>) {
-  const { level, id, baseClass = "heading", modifiers, url, urlClass, text, blockName, attributes } = props;
+  const { level, id, baseClass, modifiers, url, urlClass, text, blockName, attributes } = props;
 
-  let heading_base_class = baseClass;
+  console.log("baseClass", baseClass);
+  let heading_base_class = baseClass ? baseClass : "heading";
+
   if (level < 1 || level > 6) {
     throw new Error("Heading only supports levels 1-6");
   }
