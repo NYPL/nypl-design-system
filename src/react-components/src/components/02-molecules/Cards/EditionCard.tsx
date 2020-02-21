8767cf6b10030be8923945e39105fd446d9b6462
Crystal Ngai <crystal.hk.ngai@gmail.com>
2020-02-21 11:43:06 -0500

    SFR-135 added a TODO comment

--- src/react-components/src/components/02-molecules/Cards/EditionCard.tsx
+++ src/react-components/src/components/02-molecules/Cards/EditionCard.tsx
@@ -11,8 +11,14 @@ export type EditionDetails = {
   coverUrl: string,
   language: string | JSX.Element,
   license: string | JSX.Element,
+
+  /** readOnlineLink and downloadLink take in either urls as a string,
+   *  link-type (<a>, <ReactRouter.Link>) elements
+   * TODO: make it so that it only takes elements.
+   * Having it take URLs is confusing and the logic shouldn't live here.*/
   readOnlineLink: string | JSX.Element,
   downloadLink: string | JSX.Element,
+
   /** Element to render when there are no links. */
   noLinkElement?: JSX.Element
 };
