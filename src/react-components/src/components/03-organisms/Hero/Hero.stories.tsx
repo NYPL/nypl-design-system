7d80ec22ff1ef2ea9d1252807340bbaabe28f24a
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-03-18 09:29:37 -0400

    [hero] Adds additional stories and background-image CSS

--- src/react-components/src/components/03-organisms/Hero/Hero.stories.tsx
+++ src/react-components/src/components/03-organisms/Hero/Hero.stories.tsx
@@ -7,8 +7,46 @@ export default {
   component: Hero,
 };
 
-export const hero = () => <Hero
+export const heroPrimary = () => <Hero
   heroPrimary={true}
+  heroId="2"
+  headerId="1"
+  headerText="Example Hero"
+  imageUrl="https://placeimg.com/1600/800/arch">
+</Hero>;
+
+export const heroPrimaryWithSubtitleAndCustomColor = () => <Hero
+  heroPrimary={true}
+  heroId="2"
+  headerId="1"
+  headerText="Staff Picks"
+  subHeaderText="Nobody loves books more than our experts. Browse and filter hundreds of their favorites. Want more suggestions? Check out <a href='#'>Best Books</a> and <a href='#'>125 Books We Love</a>."
+  foregroundColor="#ffffff"
+  backgroundColor="#bb1d12"
+  imageUrl="https://placeimg.com/1600/800/arch">
+</Hero>;
+
+export const heroPrimaryWithLocationDetails = () => <Hero
+  heroPrimary={true}
+  heroId="2"
+  headerId="1"
+  headerText="Syncretic Vibrations: Exploring the Mosaic of Blackness through the Melville J. and Frances S.Herskovits Collection"
+  foregroundColor="#ffffff"
+  backgroundColor="#0071ce"
+  imageUrl="https://images.nypl.org/index.php?id=419627&t=w">
+</Hero>;
+
+export const heroSecondary = () => <Hero
+  heroPrimary={false}
+  heroId="2"
+  headerId="1"
+  headerText="Title Of Page"
+  imageUrl="https://placeimg.com/200/100/arch">
+</Hero>;
+
+export const heroTertiary = () => <Hero
+  heroPrimary={false}
+  heroId="2"
   headerId="1"
   headerText="Title Of Page"
   imageUrl="https://placeimg.com/200/100/arch">
--- src/react-components/src/components/03-organisms/Hero/Hero.tsx
+++ src/react-components/src/components/03-organisms/Hero/Hero.tsx
@@ -1,23 +1,28 @@
 // OH-30 Header with Image Right
 import * as React from "react";
 import bem from "../../../utils/bem";
-import Image from "../../01-atoms/Images/Image/Image";
 import BodyText from "../../01-atoms/Text/Text/BodyText";
+import Image from "../../01-atoms/Images/Image/Image";
 import Heading from "../../01-atoms/Text/Headings/Heading";
 
 export interface HeroProps {
   heroPrimary: boolean;
+  heroId: string;
 
   headerId: string;
   headerText: string;
+
   subHeaderText?: string;
+
+  foregroundColor?: string;
+  backgroundColor?: string;
 
   imageUrl: string;
   imageAltText?: string;
   imgAttributes?: {};
   imageModifiers?: string[];
 
-  bodyText?: JSX.Element;
+  additionalDetails?: JSX.Element;
 }
 
 export default function Hero(props: React.PropsWithChildren<HeroProps>) {
@@ -26,30 +31,66 @@ export default function Hero(props: React.PropsWithChildren<HeroProps>) {
 
   let {
     heroPrimary, 
+    heroId, 
 
     headerId, 
     headerText, 
 
+    subHeaderText,
+
+    foregroundColor,
+    backgroundColor,
+
     imageUrl,
+    additionalDetails,
   } = props;
 
+  // Set imageUrl as background if it's a primary hero
+  // else, display normally
   let heroModifiers = heroPrimary ? ["primary"] : [];
 
-  let imageModifiers = heroPrimary ? ["background"] : [];
+  let backgroundImage = heroPrimary ? "data-responsive-background-image" : "";
+
+  let backgroundImageStyle = heroPrimary ? {backgroundImage: 'url(' + imageUrl + ')'} : {};
+
+  let image = heroPrimary ? 
+    '' : <Image 
+      src={imageUrl} 
+      isDecorative={true} 
+      imageBlockname={heroBaseClass} 
+    ></Image>;
+
+  function createMarkup() {
+    return {__html: subHeaderText};
+  }
+
+  let headersStyling;
+
+  if (foregroundColor && backgroundColor) {
+    headersStyling = {
+      color: foregroundColor,
+      backgroundColor: backgroundColor,
+    }
+  } else {
+    console.log("Foreground and background not provided");
+    headersStyling = {};
+  }
 
   return (
-    <div className={bem(heroBaseClass, heroModifiers)}>
-      <Heading level={1} id={headerId}
-        blockName="hero"
-        text={headerText}>
-      </Heading>
+    <div className={bem(heroBaseClass, heroModifiers)} data-responsive-background-image id={heroId} style={backgroundImageStyle}>
 
-      <Image 
-        src={imageUrl} 
-        isDecorative={true} 
-        imageBlockname={heroBaseClass} 
-        imageModifiers={imageModifiers} 
-      ></Image>
+      <div className={bem('headers', [], heroBaseClass)} style={headersStyling}>
+        <Heading level={1} id={headerId}
+          blockName="hero"
+          text={headerText}>
+        </Heading>
+
+        <p className={bem('subtitle', [], heroBaseClass)} dangerouslySetInnerHTML={createMarkup()} />
+      </div>
+
+      {image}
+
+      {/* <BodyText bodyContent={additionalDetails}></BodyText> */}
     </div>
   );
 }
