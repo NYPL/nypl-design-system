883db9984d3f97de2161e7caef3d71a6b0f53402
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-04-01 14:33:28 -0400

    [hero] Adds description of Placeholder component

--- src/react-components/src/components/00-base/Placeholder.tsx
+++ src/react-components/src/components/00-base/Placeholder.tsx
@@ -7,6 +7,10 @@ export interface PlaceholderProps {
   blockName?: string;
 }
 
+/** Creates a gray box with an editable string for dropping in spaces
+ * within components or on templates where a component will be but 
+ * that hasn't been created yet
+ */
 export default function Placeholder(props: React.PropsWithChildren<PlaceholderProps>) {
   const { modifiers = [], blockName } = props;
   return (
