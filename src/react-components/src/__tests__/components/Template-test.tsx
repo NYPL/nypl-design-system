cfb59d892093af5a5bd5b8b93977d052be7e9b74
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-05-18 11:58:57 -0400

    [SFR-894] First test

--- src/react-components/src/__tests__/components/Template-test.tsx
+++ src/react-components/src/__tests__/components/Template-test.tsx
@@ -2,11 +2,24 @@ import { expect } from "chai";
 import { stub } from "sinon";
 import * as Enzyme from "enzyme";
 import * as React from "react";
+import Template from "../../components/Template/Template";
+import { LayoutTypes } from "../../components/Template/LayoutTypes";
+import Placeholder from "../../components/00-base/Placeholder";
 
 import * as Mocha from "mocha";
 
-describe("Template", () => {
+describe("Template Test", () => {
   let wrapper: Enzyme.ShallowWrapper<{}, {}>;
+
+  it("Generates a full width Template", () => {
+    let wrapper = Enzyme.shallow(<Template
+      layoutType={LayoutTypes.FullWidth}
+      headerContent={<Placeholder>Header</Placeholder>}
+      primaryContent={<Placeholder>Main Content</Placeholder>}
+      footerContent={<Placeholder>Footer</Placeholder>}
+    ></Template>);
+    expect(wrapper.prop("className")).to.equal("layout-container");
+  });
 });
 
 // Generates "default template", the skeleton
