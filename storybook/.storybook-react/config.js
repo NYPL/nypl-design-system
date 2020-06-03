b6b6acbdf00b96fb9496a1d5a4c63b750b12bf9a
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-06-03 14:45:28 -0400

    Setting back up style.scss file

--- storybook/.storybook-react/config.js
+++ storybook/.storybook-react/config.js
@@ -1,8 +1,8 @@
-import '!style-loader!css-loader!sass-loader!import-glob-loader!@nypl/design-system-styles/style.scss';
-import { configure, addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
+import "!style-loader!css-loader!sass-loader!import-glob-loader!@nypl/design-system-react-components/style.scss";
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 
 // Add all the addons when starting up.
 addDecorator(withInfo);
@@ -12,4 +12,11 @@ addDecorator(withKnobs);
 // automatically import all stories from node_modules/stories ending in *.stories.js
 // Direct link used instead of require(module) because of Webpack's inability to resolve variables in require.context
 
-configure(require.context('../../node_modules/@nypl/design-system-react-components/src/components', true, /\.stories\.tsx$/), module);
+configure(
+    require.context(
+        "../../node_modules/@nypl/design-system-react-components/src/components",
+        true,
+        /\.stories\.tsx$/
+    ),
+    module
+);
