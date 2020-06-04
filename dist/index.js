07e3d937fe116f170b0177d8fa12b52bc9fd872f
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-06-04 18:26:46 +0200

    remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./root.cjs.production.min.js')
-} else {
-  module.exports = require('./root.cjs.development.js')
-}
