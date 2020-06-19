4e5fa315736f16a0cfd00ad2b4bc1c2efdcfe29d
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-06-19 14:33:12 -0400

    Fixes ShowInfo error message

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -7,14 +7,16 @@ export default {
     component: Heading,
 };
 
-export const allHeadings = () => [
-    <Heading level={1} id="heading1" text={"Heading 1"} />,
-    <Heading level={2} id="heading2" text={"Heading 2"} />,
-    <Heading level={3} id="heading3" text={"Heading 3"} />,
-    <Heading level={4} id="heading4" text={"Heading 4"} />,
-    <Heading level={5} id="heading5" text={"Heading 5"} />,
-    <Heading level={6} id="heading6" text={"Heading 6"} />,
-];
+export const allHeadings = () => (
+    <>
+        <Heading level={1} id="heading1" text={"Heading 1"} />
+        <Heading level={2} id="heading2" text={"Heading 2"} />
+        <Heading level={3} id="heading3" text={"Heading 3"} />
+        <Heading level={4} id="heading4" text={"Heading 4"} />
+        <Heading level={5} id="heading5" text={"Heading 5"} />
+        <Heading level={6} id="heading6" text={"Heading 6"} />
+    </>
+);
 
 allHeadings.story = {
     name: "All Headings",
