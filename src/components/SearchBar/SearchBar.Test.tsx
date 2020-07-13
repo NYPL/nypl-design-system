c11894c1d543980f8548f7e87d78aa83c7f438ac
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-07-13 16:39:57 -0400

    [searchbar] Updates tests

--- src/components/SearchBar/SearchBar.Test.tsx
+++ src/components/SearchBar/SearchBar.Test.tsx
@@ -3,99 +3,46 @@ import { stub } from "sinon";
 import * as Enzyme from "enzyme";
 import * as React from "react";
 
+import Button from "../Button/Button";
+import { ButtonTypes } from "../Button/ButtonTypes";
 import SearchBar from "./SearchBar";
 
-describe("Search Bar with Optional Parameters", () => {
+describe("SearchBar", () => {
     let wrapper: Enzyme.ShallowWrapper<{}, {}>;
-    let searchCallback;
-    let onChangeCallback;
+    let searchBarSubmit, buttonCallback;
     beforeEach(() => {
-        searchCallback = stub();
-        onChangeCallback = stub();
+        searchBarSubmit = stub();
+        (buttonCallback) => searchBarSubmit();
     });
 
-    it("Search Bar has an input field and button", () => {
+    it("Renders SearchBar", () => {
         let wrapper = Enzyme.shallow(
-            <SearchBar id="id" ariaLabel="label" onSubmit={searchCallback} />
+            <SearchBar
+                id="id"
+                ariaLabel="searchbar"
+                onSubmit={searchBarSubmit}
+            />
         );
-        expect(wrapper.find("TextField")).to.have.lengthOf(1);
-        expect(wrapper.find("Button")).to.have.lengthOf(1);
+        expect(wrapper.find(".searchbar")).to.have.lengthOf(1);
+        expect(wrapper.find(".searchbar").prop("aria-label")).to.equal(
+            "searchbar"
+        );
     });
 
-    it("Search Bar calls callback on Submit ", () => {
+    it("Search Bar calls call back on Submit ", () => {
         let mounted = Enzyme.mount(
-            <SearchBar
-                ariaLabel="searchBar"
-                id="id"
-                onSubmit={searchCallback}
-            />
+            <SearchBar ariaLabel="searchBar" id="id" onSubmit={searchBarSubmit}>
+                <Button
+                    buttonType={ButtonTypes.Primary}
+                    id="button"
+                    onClick={buttonCallback}
+                    type="submit"
+                >
+                    Search
+                </Button>
+            </SearchBar>
         );
         mounted.find("Button").simulate("click");
-        expect(searchCallback.callCount).to.equal(1);
-    });
-
-    it("SearchBar calls optional onChange property", () => {
-        wrapper = Enzyme.shallow(
-            <SearchBar
-                ariaLabel="searchBar"
-                id="id"
-                onSubmit={searchCallback}
-            />
-        );
-        wrapper
-            .find("TextField")
-            .dive()
-            .find("input")
-            .simulate("change", { target: { value: "Hello" } });
-        expect(onChangeCallback.callCount).to.equal(1);
-    });
-
-    it("Shows 'error' span when error is passed", () => {
-        wrapper = Enzyme.shallow(
-            <SearchBar
-                id="id"
-                ariaLabel="label"
-                errored={true}
-                onSubmit={searchCallback}
-            />
-        );
-        expect(
-            wrapper.find(".search-bar__input-description--error")
-        ).to.have.lengthOf(1);
-    });
-
-    it("Shows 'error' text inside input when error is passed with ResearchNow Variation", () => {
-        let mounted = Enzyme.mount(
-            <SearchBar
-                id="id"
-                ariaLabel="label"
-                errored={true}
-                onSubmit={searchCallback}
-            />
-        );
-
-        expect(
-            mounted.find(".search-bar__input-description--error")
-        ).to.have.lengthOf(0);
-        expect(mounted.find("input").prop("placeholder")).to.equal("test");
-    });
-
-    it("Throws error when SearchBar is not passed aria-label or aria-labelledby", () => {
-        expect(() =>
-            Enzyme.mount(<SearchBar id="id" onSubmit={searchCallback} />)
-        ).to.throw("Either ariaLabel and ariaLabelledBy must be passed");
-    });
-
-    it("Throws error when errored is true and no errorMessage is passed", () => {
-        expect(() =>
-            Enzyme.mount(
-                <SearchBar
-                    id="id"
-                    ariaLabel="label"
-                    errored={true}
-                    onSubmit={searchCallback}
-                />
-            )
-        ).to.throw("If there is an error, there must also be an error message");
+        expect(searchBarSubmit.callCount).to.equal(1);
     });
 });
