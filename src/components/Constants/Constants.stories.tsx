27fa57b993b2ad29c61482e1ce226aba68a39e85
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-07-24 09:33:38 -0400

    Exposes type variables

--- src/components/Constants/Constants.stories.tsx
+++ src/components/Constants/Constants.stories.tsx
@@ -124,7 +124,7 @@ utilityColors.story = {
     },
 };
 
-const getSetCSSVariable = function (name: string) {
+const getCSSVariable = function (name: string) {
     let propertyValue = "";
     propertyValue = getComputedStyle(document.documentElement).getPropertyValue(
         name
@@ -144,22 +144,22 @@ export const brandFills = () => (
         >
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-whats-on-primary"
                     )}
                 >
                     --section-whats-on-primary:{" "}
-                    {getSetCSSVariable("--section-whats-on-primary")}
+                    {getCSSVariable("--section-whats-on-primary")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-whats-on-secondary"
                     )}
                 >
                     --section-whats-on-secondary:{" "}
-                    {getSetCSSVariable("--section-whats-on-secondary")}
+                    {getCSSVariable("--section-whats-on-secondary")}
                 </UIDocCard>
             </li>
         </List>
@@ -175,22 +175,22 @@ export const brandFills = () => (
         >
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-books-and-more-primary"
                     )}
                 >
                     --section-books-and-more-primary:{" "}
-                    {getSetCSSVariable("--section-books-and-more-primary")}
+                    {getCSSVariable("--section-books-and-more-primary")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-books-and-more-secondary"
                     )}
                 >
                     --section-books-and-more-secondary:{" "}
-                    {getSetCSSVariable("--section-books-and-more-secondary")}
+                    {getCSSVariable("--section-books-and-more-secondary")}
                 </UIDocCard>
             </li>
         </List>
@@ -203,22 +203,22 @@ export const brandFills = () => (
         >
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-research-primary"
                     )}
                 >
                     --section-research-primary:{" "}
-                    {getSetCSSVariable("--section-research-primary")}
+                    {getCSSVariable("--section-research-primary")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-research-secondary"
                     )}
                 >
                     --section-research-secondary:{" "}
-                    {getSetCSSVariable("--section-research-secondary")}
+                    {getCSSVariable("--section-research-secondary")}
                 </UIDocCard>
             </li>
         </List>
@@ -234,22 +234,22 @@ export const brandFills = () => (
         >
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-locations-primary"
                     )}
                 >
                     --section-locations-primary:{" "}
-                    {getSetCSSVariable("--section-locations-primary")}
+                    {getCSSVariable("--section-locations-primary")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-locations-secondary"
                     )}
                 >
                     --section-locations-secondary:{" "}
-                    {getSetCSSVariable("--section-locations-secondary")}
+                    {getCSSVariable("--section-locations-secondary")}
                 </UIDocCard>
             </li>
         </List>
@@ -262,34 +262,32 @@ export const brandFills = () => (
         >
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-research-library-lpa"
                     )}
                 >
                     --section-research-library-lpa:{" "}
-                    {getSetCSSVariable("--section-research-library-lpa")}
+                    {getCSSVariable("--section-research-library-lpa")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-research-library-schomburg"
                     )}
                 >
                     --section-research-library-schomburg:{" "}
-                    {getSetCSSVariable("--section-research-library-schomburg")}
+                    {getCSSVariable("--section-research-library-schomburg")}
                 </UIDocCard>
             </li>
             <li style={{ textAlign: "center" }}>
                 <UIDocCard
-                    backgroundColor={getSetCSSVariable(
+                    backgroundColor={getCSSVariable(
                         "--section-research-library-schwartzman"
                     )}
                 >
                     --section-research-library-schwartzman:{" "}
-                    {getSetCSSVariable(
-                        "--section-research-library-schwartzman"
-                    )}
+                    {getCSSVariable("--section-research-library-schwartzman")}
                 </UIDocCard>
             </li>
         </List>
@@ -303,6 +301,96 @@ brandFills.story = {
             type: "figma",
             url:
                 "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=15454%3A47007",
+        },
+    },
+};
+
+export const typefaces = () => (
+    <>
+        <Heading level={1}>Typefaces</Heading>
+        <p>
+            System Font, --typeface-body: {getCSSVariable("--typeface-body")};
+        </p>
+        <Heading level={1}>Weights</Heading>
+        <p style={{ fontWeight: 700 }}>Bold / 700</p>
+        <p style={{ fontWeight: 500 }}>Medium / 500</p>
+        <p style={{ fontWeight: 400 }}>Regular / 400</p>
+        <p style={{ fontWeight: 300 }}>Light / 300</p>
+
+        <p>
+            White, percolator aroma, trifecta, doppio blue mountain eu con panna
+            kopi-luwak café au lait. Pumpkin spice galão breve skinny so sugar
+            cream seasonal White, percolator aroma, trifecta, doppio blue
+            mountain eu con panna kopi-luwak café au lait. Pumpkin spice galão
+            breve skinny so sugar cream seasonal White, percolator aroma,
+            trifecta, doppio blue mountain eu con panna kopi-luwak café au lait.
+            Pumpkin spice galão breve con panna kopi-luwak café au lait Pumpkin
+            spice galão breve Pumpkin spicePumpkin spice galão breve con panna
+            kopi-luwak café au lait Pumpkin spice galão breve Pumpkin spice.
+        </p>
+    </>
+);
+
+typefaces.story = {
+    name: "Typefaces",
+    parameters: {
+        design: {
+            type: "figma",
+            url:
+                "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=10975%3A0",
+        },
+    },
+};
+
+let headingSizes = ["Callout", "Tertiary", "Secondary", "Primary"];
+
+let headings = [];
+
+for (let i = headingSizes.length; i--; i >= 1) {
+    headings.push(
+        <p>
+            <span
+                style={{
+                    fontSize: `var(--font-size-${i + 1})`,
+                    marginRight: "var(--space-s)",
+                }}
+            >
+                {headingSizes[i]}
+            </span>
+            <span>
+                --font-size-{i + 1}: {getCSSVariable(`--font-size-${i}`)}
+            </span>
+        </p>
+    );
+}
+
+export const typeScale = () => (
+    <>
+        <Heading level={1}>Type Scale</Heading>
+        <Heading level={2}>Display Sizes</Heading>
+        <p>
+            Used for Headings on pages and callouts. These are separate from
+            semantic elements—an <code>h1</code> has the "Headline Primary"
+            style applied out of the box, for example, but the Headline Primary
+            style can be applied to any type element on the page.
+        </p>
+        <>{headings}</>
+
+        <Heading level={1}>Body Styles</Heading>
+        <p style={{ fontSize: "var(--font-size-0)" }}>Body (0)</p>
+        <p style={{ fontSize: "var(--font-size--1)" }}>-1</p>
+        <p style={{ fontSize: "var(--font-size--2)" }}>-2</p>
+        <p style={{ fontSize: "var(--font-size--3)" }}>-3</p>
+    </>
+);
+
+typefaces.story = {
+    name: "Typefaces",
+    parameters: {
+        design: {
+            type: "figma",
+            url:
+                "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=10975%3A16",
         },
     },
 };
