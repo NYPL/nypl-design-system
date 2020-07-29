ac7bc0d15754ee2b9a99095e8e6b900a2f23cc14
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-07-29 15:19:16 -0400

    Split up CSSVariablesHelper functions

--- src/helpers/CSSVariablesHelper.tsx
+++ src/helpers/CSSVariablesHelper.tsx
@@ -1,27 +1,31 @@
-let cssVariables: {
-    [k: string]: string;
-} = {};
+import { CSSVariablesInterface } from "../interfaces";
+
+const cssVariables: CSSVariablesInterface = {};
+
+const buildCSSObject = function (prev, cssRule) {
+    // All of our global variables are applied to :root
+    if (cssRule.selectorText == ":root") {
+        var css = cssRule.cssText.split("{");
+        css = css[1].replace("}", "").split(";");
+        for (var i = 0; i < css.length; i++) {
+            var prop = css[i].split(":");
+            if (
+                // if a valid CSS variable, i.e. --[whatever]
+                prop.length == 2 &&
+                prop[0].indexOf("--") == 1
+            ) {
+                cssVariables[prop[0]] = prop[1];
+            }
+        }
+    }
+};
 
 [].slice.call(document.styleSheets).reduce(function (prev, styleSheet) {
     if (styleSheet.cssRules) {
         return (
             prev +
             [].slice.call(styleSheet.cssRules).reduce(function (prev, cssRule) {
-                // All of our global variables are applied to :root
-                if (cssRule.selectorText == ":root") {
-                    var css = cssRule.cssText.split("{");
-                    css = css[1].replace("}", "").split(";");
-                    for (var i = 0; i < css.length; i++) {
-                        var prop = css[i].split(":");
-                        if (
-                            // if a valid CSS variable, i.e. --[whatever]
-                            prop.length == 2 &&
-                            prop[0].indexOf("--") == 1
-                        ) {
-                            cssVariables[prop[0]] = prop[1];
-                        }
-                    }
-                }
+                buildCSSObject(prev, cssRule);
             }, "")
         );
     }
