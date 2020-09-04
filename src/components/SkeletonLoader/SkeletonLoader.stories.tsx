87b4d7731cbecb5a2b0cb7abb9d2ddb95704d32a
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-09-04 11:52:13 -0400

    Closes #236

---
+++ src/components/SkeletonLoader/SkeletonLoader.stories.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import SkeletonLoader from "./SkeletonLoader";
+import { withDesign } from "storybook-addon-designs";
+
+export default {
+    title: "Skeleton Loader",
+    component: SkeletonLoader,
+    decorators: [withDesign],
+};
+
+export const skeletonLoader = () => <SkeletonLoader />;
+
+skeletonLoader.story = {
+    name: "Skeleton Loader",
+    parameters: {
+        design: {
+            type: "figma",
+            url:
+                "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=17219%3A7",
+        },
+    },
+};
