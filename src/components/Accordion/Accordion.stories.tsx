c211bfa1e04fd654e0e9fbe8adac002a068cd880
Helen V. Holmes <helen.v.holmes@gmail.com>
2020-09-09 11:49:50 -0400

    Update hover and font styles to match designs

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -4,7 +4,7 @@ import Accordion, { AccordionProps } from "./Accordion";
 import { action } from "@storybook/addon-actions";
 import { withDesign } from "storybook-addon-designs";
 
-import Checkbox from "../Checkbox/Checkbox";
+import { BasicCheckbox } from "../Checkbox/Checkbox.stories";
 import List from "../List/List";
 import { ListTypes } from "../List/ListTypes";
 
@@ -16,63 +16,58 @@ export default {
 
 // Set up the reusable template to create multiple stories for the
 // Accordian component.
-const Template: Story<AccordionProps> = (args) => (
-    <Accordion {...args}>{childrenList}</Accordion>
-);
-
-const checkboxes = [
-    <li key="one">
-        <Checkbox
-            onChange={action("changed")}
-            checkboxId="checkbox-1"
-            labelOptions={{
-                id: "checkbox-1-label",
-                labelContent: <>Checkbox 1</>,
-            }}
-        ></Checkbox>
-    </li>,
-    <li key="two">
-        <Checkbox
-            onChange={action("changed")}
-            checkboxId="checkbox-2"
-            labelOptions={{
-                id: "checkbox-2-label",
-                labelContent: <>Checkbox 2</>,
-            }}
-        ></Checkbox>
-    </li>,
-    <li key="three">
-        <Checkbox
-            onChange={action("changed")}
-            checkboxId="checkbox-3"
-            labelOptions={{
-                id: "checkbox-3-label",
-                labelContent: <>Checkbox 3</>,
-            }}
-        ></Checkbox>
-    </li>,
-];
-const childrenList = (
-    <List
-        type={ListTypes.Unordered}
-        id="checkbox-list"
-        modifiers={["no-list-styling"]}
-    >
-        {checkboxes}
-    </List>
+const AccordionTemplate = ({ items, ...args }) => (
+    <Accordion {...args}>
+        <List
+            type={ListTypes.Unordered}
+            id="checkbox-list"
+            modifiers={["no-list-styling"]}
+        >
+            {items.map((item) => (
+                <li>
+                    <BasicCheckbox {...item} />
+                </li>
+            ))}
+        </List>
+    </Accordion>
 );
 
 // This is one specific story where it will render a list of checkboxes.
-export const AccordionWithCheckboxes = Template.bind({});
+export const AccordionWithCheckboxes = AccordionTemplate.bind({});
 
 // The `args` allow these props to be updated in the UI through the
 // "Controls" tab.
 AccordionWithCheckboxes.args = {
     accordionLabel: "Click to expand",
     labelId: "accordionBtn",
+    items: [BasicCheckbox.args],
+};
+
+AccordionWithCheckboxes.argTypes = {
+    blockName: { table: { disable: true } },
+    className: { table: { disable: true } },
+    id: { table: { disable: true } },
+    modifiers: { table: { disable: true } },
 };
 
 AccordionWithCheckboxes.parameters = {
+    design: {
+        type: "figma",
+        url:
+            "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=10734%3A2520",
+    },
+};
+
+// This is one specific story where it will render a list of FAQs.
+export const AccordionAsFAQ = AccordionTemplate.bind({});
+AccordionAsFAQ.args = {
+    accordionLabel: "FAQ Question Lorem Ipsum",
+    modifiers: ["faq"],
+    labelId: "accordionBtn",
+    items: [BasicCheckbox.args],
+};
+
+AccordionAsFAQ.parameters = {
     design: {
         type: "figma",
         url:
