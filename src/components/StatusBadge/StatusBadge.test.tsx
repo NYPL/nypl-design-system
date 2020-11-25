5d8c1557387301f5c3adebd06f9cbee9f5b1dbb0
Milos Pumpalovic <mip2047@med.cornell.edu>
2020-11-24 22:01:48 -0500

    RENO-1882 - Revise Status Badge Component

---
+++ src/components/StatusBadge/StatusBadge.test.tsx
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import * as Enzyme from "enzyme";
+import * as React from "react";
+import * as Mocha from "mocha";
+
+import StatusBadge from "./StatusBadge";
+
+describe("StatusBadge Test", () => {
+    let wrapper: Enzyme.ShallowWrapper<{}, {}>;
+
+    it("Shows status badge", () => {
+        wrapper = Enzyme.shallow(
+            <StatusBadge type={'notice'} text={"Registration Required"} />
+        );
+        expect(wrapper.find("div")).to.have.lengthOf(1);
+    });
+    it("Can pass status badge content as child", () => {
+        wrapper = Enzyme.shallow(
+            <StatusBadge type={'alert'}>
+                Cancelled
+            </StatusBadge>
+        );
+        expect(wrapper.find("div")).to.have.lengthOf(1);
+    });
+});
---
+++ src/components/StatusBadge/StatusBadge.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import bem from "../../utils/bem";
+
+export interface StatusBadgeProps {
+    /** BlockName for use with BEM. See how to work with blockNames and BEM here: http://getbem.com/introduction/ */
+    blockName?: string;
+    /** ClassName that appears in addition to "status-badge" */
+    className?: string;
+    /** Inner text of the element */
+    text?: string;
+    /** Type of badge **/
+    type?: string;
+}
+
+export default function Heading(props: React.PropsWithChildren<StatusBadgeProps>) {
+    const {
+        blockName,
+        className,
+        text,
+        type,
+    } = props;
+
+    let status_badge_base_class = "heading";
+
+    if (!props.children && !text) {
+        throw new Error("Status Badge has no children, please pass prop: text");
+    }
+
+    let statusBadgeModifiers = ['warning'];
+
+    if (type && type == 'notice') {
+        statusBadgeModifiers = [type];
+    } else {
+        statusBadgeModifiers = ['warning'];
+    }
+
+    let content;
+
+    if (props.children) {
+        content = props.children;
+    } else {
+        content = text;
+    }
+
+    return(
+        <div
+            className={bem(status_badge_base_class, statusBadgeModifiers, blockName, [className])}
+        >
+            {content}
+        </div>
+    );
+}
