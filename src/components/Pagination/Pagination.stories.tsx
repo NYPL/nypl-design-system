6e99b7399ad3a6184d6edd22190cca9cf5527b56
Matt Misselbeck <matt@misselbeck.com>
2020-11-24 21:39:59 -0500

    Add an initial story for review

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -1,16 +1,8 @@
 import * as React from "react";
-import { action } from "@storybook/addon-actions";
-import { withDesign } from "storybook-addon-designs";
-import { text } from "@storybook/addon-knobs";
 import { Story } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
-import Button from "../Button/Button";
-import { ButtonTypes } from "../Button/ButtonTypes";
-import Icon from "../Icons/Icon";
-import { IconNames, IconRotationTypes } from "../Icons/IconTypes";
-import Label from "../Label/Label";
 import Pagination, { PaginationProps } from "./Pagination";
-import Select from "../Select/Select";
 
 export default {
     title: "Pagination",
@@ -18,82 +10,20 @@ export default {
     decorators: [withDesign],
 };
 
-const selectOptions = (
-    <>
-        <Label htmlFor="paginationSelect" id={"paginationLabel"}>
-            {text("Select Label", "Page ")}
-        </Label>
-        <Select
-            name="Pagination Select"
-            id={"paginationSelect"}
-            isRequired={false}
-            ariaLabel="Pagination Label"
-            labelId={"paginationLabel"}
-            onBlur={action("blur")}
-            onChange={action("changed")}
-            selectedOption={"1 of 7"}
-        >
-            <option aria-selected={true}>
-                {text("Option 1 Text", "1 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 2 Text", "2 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 3 Text", "3 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 4 Text", "4 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 5 Text", "5 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 6 Text", "6 of 7")}
-            </option>
-            <option aria-selected={false}>
-                {text("Option 7 Text", "7 of 7")}
-            </option>
-        </Select>
-    </>
-);
-
-const PaginationTemplate = ({ children, ...args }) => (
-    <Pagination {...args}>{children}</Pagination>
+const PaginationTemplate: Story<PaginationProps> = (args) => (
+    <Pagination {...args}></Pagination>
 );
 
 export const pagination = PaginationTemplate.bind({});
 
 pagination.args = {
-    previousPage: (
-        <Button
-            buttonType={ButtonTypes.Secondary}
-            onClick={action("Previous clicked")}
-        >
-            <Icon
-                name={IconNames.arrow}
-                decorative={true}
-                iconRotation={IconRotationTypes.rotate90}
-                modifiers={["small", "icon-left"]}
-            />
-            {text("Previous Button Label", "Previous")}
-        </Button>
-    ),
-    nextPage: (
-        <Button
-            buttonType={ButtonTypes.Secondary}
-            onClick={action("Next clicked")}
-        >
-            {text("Next Button Label", "Next")}
-            <Icon
-                name={IconNames.arrow}
-                decorative={true}
-                iconRotation={IconRotationTypes.rotate270}
-                modifiers={["small", "icon-right"]}
-            />
-        </Button>
-    ),
-    children: selectOptions,
+    pageCount: 10,
+};
+
+pagination.argTypes = {
+    blockName: { table: { disable: true } },
+    className: { table: { disable: true } },
+    modifiers: { table: { disable: true } },
 };
 
 pagination.storyName = "Pagination";
@@ -103,10 +33,4 @@ pagination.parameters = {
         url:
             "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=17226%3A932",
     },
-};
-
-pagination.argTypes = {
-    previousPage: { table: { disable: true } },
-    nextPage: { table: { disable: true } },
-    children: { table: { disable: true } },
 };
