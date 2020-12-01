cc24534637c159a221a18b3c03d2eb7859678b0b
wluisi <w.luisi@williamluisi.org>
2020-12-01 14:13:06 -0500

    updates for storybook

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -3,26 +3,57 @@ import React, { useState } from "react";
 import Checkbox, { CheckboxProps } from "./Checkbox";
 import { Story } from "@storybook/react/types-6-0";
 
+// Set defaults for the checkbox stories.
 export default {
     title: "Checkbox",
     component: Checkbox,
+    // Remove some props for all checkbox stories.
+    argTypes: {
+        checkboxId: {
+            table: {
+                disable: true,
+            },
+        },
+        onChange: {
+            table: {
+                disable: true,
+            },
+        },
+        labelOptions: {
+            table: {
+                disable: true,
+            },
+        },
+    },
 };
 
 const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
 
-export const BasicCheckbox = Template.bind({});
-BasicCheckbox.args = {
+// Uncontrolled checkbox.
+export const UncontrolledCheckbox = Template.bind({});
+
+// Uncontrolled checkbox set props
+UncontrolledCheckbox.args = {
     checkboxId: "checkbox",
     defaultChecked: false,
     labelOptions: {
         id: "label",
-        labelContent: <>This is a basic uncontrolled checkbox.</>,
+        labelContent: <>This is an uncontrolled checkbox.</>,
     },
     onChange: () => {},
 };
 
-// @TODO Add properties to "controls"
-export const ControlledCheckbox = () => {
+// Uncontrolled checkbox set controls
+UncontrolledCheckbox.argTypes = {
+    checked: {
+        table: {
+            disable: true,
+        },
+    },
+};
+
+// Controlled checkbox.
+const ControlledCheckboxTemplate: Story<CheckboxProps> = (args) => {
     const [isChecked, setChecked] = useState<boolean>(false);
 
     const onChange = (event) => {
@@ -31,16 +62,33 @@ export const ControlledCheckbox = () => {
 
     return (
         <Checkbox
+            {...args}
             checkboxId={"checkbox"}
             checked={isChecked}
             aria-checked={isChecked}
             labelOptions={{
                 id: "label",
-                labelContent: (
-                    <>This is a basic controlled checkbox. using state.</>
-                ),
+                labelContent: <>This is a controlled checkbox. using state.</>,
             }}
             onChange={onChange}
         />
     );
+};
+
+export const ControlledCheckbox = ControlledCheckboxTemplate.bind({});
+
+ControlledCheckbox.args = {
+    checked: true,
+};
+
+// Controlled checkbox set controls
+ControlledCheckbox.argTypes = {
+    checked: {
+        type: "boolean",
+    },
+    defaultChecked: {
+        table: {
+            disable: true,
+        },
+    },
 };
