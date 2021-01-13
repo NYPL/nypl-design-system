7d3ec75fdb5c2a6cfe38684bcd1ba98d7ab99210
hokei <3579472+hokei@users.noreply.github.com>
2021-01-13 14:40:42 +0000

    Deploying to gh-pages from  @ ab9dfa0d9cbbfaff4a8ef93006052f8b235dbe28 🚀

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 
 import Select from "./Select";
 import Label from "../Label/Label";
@@ -15,71 +15,77 @@ const optionsGroup = ["Apples", "Pears", "Oranges", "Peaches", "Coconuts"];
 
 let showLabel, showHelperText;
 
-export const selectWithOptionalLabel = () => (
-  <>
-    {boolean("Show Label", true) ? (showLabel = true) : (showLabel = false)}
-    {showLabel && (
-      <Label
-        htmlFor="select"
-        optReqFlag={select(
-          "Optional/Required Flag",
-          ["Required", "Optional", ""],
-          "Required"
-        )}
-        id={"label"}
-      >
-        {text("Select Label", "Fruit on your island: ")}
-      </Label>
-    )}
-    <Select
-      name="optionalLabelSelect"
-      id={"select"}
-      isRequired={false}
-      ariaLabel="Select Label"
-      labelId={"label"}
-      helperTextId={"helperText"}
-      selectedOption={select(
-        "Initial Selected Option",
-        optionsGroup,
-        optionsGroup[3]
-      )}
-      onBlur={action("blur")}
-      onChange={action("changed")}
-      modifiers={boolean("Errored", false) ? ["error"] : null}
-      disabled={boolean("Disabled", false)}
-    >
-      <option aria-selected={true}>
-        {text("Option One", optionsGroup[0])}
-      </option>
-      <option aria-selected={false}>
-        {text("Option Two", optionsGroup[1])}
-      </option>
-      <option aria-selected={false}>
-        {text("Option Three", optionsGroup[2])}
-      </option>
-      <option aria-selected={false}>
-        {text("Option Four", optionsGroup[3])}
-      </option>
-      <option aria-selected={false}>
-        {text("Option Five", optionsGroup[4])}
-      </option>
-    </Select>
-    {boolean("Show Helper Text", true)
-      ? (showHelperText = true)
-      : (showHelperText = false)}
-    {showHelperText && (
-      <HelperErrorText isError={boolean("Is Errored", false)} id={"helperText"}>
-        {text(
-          "Helper Text Content",
-          "Once you pick a fruit for your island it cannot be changed, so choose wisely!"
-        )}
-      </HelperErrorText>
-    )}
-  </>
-);
+export const SelectWithOptionalLabel = () => {
+  const [selectedOption, setSelectedOption] = useState(
+    select("Initial Selected Option", optionsGroup, optionsGroup[3])
+  );
 
-selectWithOptionalLabel.storyName = "Select";
-selectWithOptionalLabel.parameters = {
+  const onChange = e => setSelectedOption(e.target.value);
+  return (
+    <>
+      {boolean("Show Label", true) ? (showLabel = true) : (showLabel = false)}
+      {showLabel && (
+        <Label
+          htmlFor="select"
+          optReqFlag={select(
+            "Optional/Required Flag",
+            ["Required", "Optional", ""],
+            "Required"
+          )}
+          id={"label"}
+        >
+          {text("Select Label", "Fruit on your island: ")}
+        </Label>
+      )}
+      <Select
+        name="optionalLabelSelect"
+        id={"select"}
+        isRequired={false}
+        ariaLabel="Select Label"
+        labelId={"label"}
+        helperTextId={"helperText"}
+        selectedOption={selectedOption}
+        onBlur={action("blur")}
+        onChange={e => onChange(e)}
+        modifiers={boolean("Errored", false) ? ["error"] : null}
+        disabled={boolean("Disabled", false)}
+      >
+        <option aria-selected={true}>
+          {text("Option One", optionsGroup[0])}
+        </option>
+        <option aria-selected={false}>
+          {text("Option Two", optionsGroup[1])}
+        </option>
+        <option aria-selected={false}>
+          {text("Option Three", optionsGroup[2])}
+        </option>
+        <option aria-selected={false}>
+          {text("Option Four", optionsGroup[3])}
+        </option>
+        <option aria-selected={false}>
+          {text("Option Five", optionsGroup[4])}
+        </option>
+      </Select>
+      {boolean("Show Helper Text", true)
+        ? (showHelperText = true)
+        : (showHelperText = false)}
+      {showHelperText && (
+        <HelperErrorText
+          isError={boolean("Is Errored", false)}
+          id={"helperText"}
+        >
+          {text(
+            "Helper Text Content",
+            "Once you pick a fruit for your island it cannot be changed, so choose wisely!"
+          )}
+        </HelperErrorText>
+      )}
+    </>
+  );
+};
+
+SelectWithOptionalLabel.storyName = "Select";
+SelectWithOptionalLabel.parameters = {
   design: {
     type: "figma",
     url:
