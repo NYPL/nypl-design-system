a42e1f923c3a5690fbeb04ad74b930836b79f629
Edwin Guzman <edwin.gzmn@gmail.com>
2021-02-19 17:03:15 -0500

    Adding padding to every story as a decorator to display the focus ring.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 // Typically, this file isn't created for a Storybook service, but we want it
 // to be able to add the custom styles.
 import "!style-loader!css-loader!sass-loader!import-glob-loader!../src/styles.scss";
+import React from "react";
 // We also want to add MDX-style documentation here:
 import { addParameters } from "@storybook/react";
 import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
@@ -11,3 +12,11 @@ addParameters({
     page: DocsPage,
   },
 });
+
+export const decorators = [
+  Story => (
+    <div style={{ margin: "10px" }}>
+      <Story />
+    </div>
+  ),
+];
