d369832f6a579fcb9e4b0706ea027754ee6d9d72
Crystal Ngai <hokei@users.noreply.github.com>
2021-03-15 15:53:17 -0400

    Implemented pagination with URL changes

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -3,15 +3,57 @@ import { Story } from "@storybook/react/types-6-0";
 import { withDesign } from "storybook-addon-designs";
 
 import Pagination, { PaginationProps } from "./Pagination";
+import { withQuery } from "@storybook/addon-queryparams";
 
 export default {
   title: "Pagination",
   component: Pagination,
-  decorators: [withDesign],
+  decorators: [withDesign, withQuery],
 };
 
-const PaginationTemplate: Story<PaginationProps> = args => {
-  const [page, setPage] = useState(7);
+const PaginationPageHrefTemplate: Story<PaginationProps> = args => {
+  const urlParams = new URLSearchParams(document.location.search);
+  const pageNumber = Number(urlParams.get("page"));
+  const location = window.location;
+
+  const getPageHref = page => {
+    const currentStoryId = urlParams.get("id");
+    return `${location.origin}?path=/story/${currentStoryId}&page=${page}`;
+  };
+  console.log("urlParams", urlParams);
+  return (
+    <Pagination
+      pageCount={args.pageCount}
+      currentPage={pageNumber}
+      getPageHref={getPageHref}
+    />
+  );
+};
+
+export const pagination = PaginationPageHrefTemplate.bind({});
+
+pagination.args = {
+  pageCount: 100,
+};
+
+pagination.argTypes = {
+  blockName: { table: { disable: true } },
+  className: { table: { disable: true } },
+  modifiers: { table: { disable: true } },
+  currentPage: { table: { disable: true } },
+};
+
+pagination.storyName = "Pagination with URL updates";
+pagination.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=17226%3A932",
+  },
+};
+
+const PaginationChangeFunctionTemplate: Story<PaginationProps> = args => {
+  const [page, setPage] = useState(args.currentPage);
 
   const pageUpdate = page => {
     setPage(page);
@@ -32,24 +74,16 @@ const PaginationTemplate: Story<PaginationProps> = args => {
   );
 };
 
-export const pagination = PaginationTemplate.bind({});
+export const paginationOnClick = PaginationChangeFunctionTemplate.bind({});
 
-pagination.args = {
+paginationOnClick.args = {
   pageCount: 100,
   currentPage: 7,
 };
 
-pagination.argTypes = {
+paginationOnClick.argTypes = {
   blockName: { table: { disable: true } },
   className: { table: { disable: true } },
   modifiers: { table: { disable: true } },
 };
-
-pagination.storyName = "Pagination";
-pagination.parameters = {
-  design: {
-    type: "figma",
-    url:
-      "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=17226%3A932",
-  },
-};
+paginationOnClick.storyName = "Pagination with unchanging URL";
--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -7,13 +7,17 @@ import Pagination from "./Pagination";
 describe("Pagination Test", () => {
   let wrapper: Enzyme.ReactWrapper<any, any>;
   let changeCallback;
+  let getPageHref;
 
   beforeEach(() => {
     changeCallback = stub();
+    getPageHref = stub();
   });
 
   it("Renders a nav element with an unordered list of items", () => {
-    wrapper = Enzyme.mount(<Pagination pageCount={11} currentPage={6} />);
+    wrapper = Enzyme.mount(
+      <Pagination pageCount={11} currentPage={6} getPageHref={getPageHref} />
+    );
     expect(wrapper.find("nav")).to.have.lengthOf(1);
     expect(wrapper.find("ul")).to.have.lengthOf(1);
     // Previous/Next buttons + truncated item list = 11 total items
@@ -21,7 +25,9 @@ describe("Pagination Test", () => {
   });
 
   it("Previous link is disabled when on the first page item", () => {
-    wrapper = Enzyme.mount(<Pagination pageCount={11} currentPage={1} />);
+    wrapper = Enzyme.mount(
+      <Pagination pageCount={11} currentPage={1} getPageHref={getPageHref} />
+    );
     expect(wrapper.find("a").first().hasClass("disabled")).to.equal(true);
     expect(
       wrapper.find("a").first().find("[aria-disabled='true']")
@@ -29,7 +35,9 @@ describe("Pagination Test", () => {
   });
 
   it("Next link is disabled when on the last page item", () => {
-    wrapper = Enzyme.mount(<Pagination pageCount={11} currentPage={11} />);
+    wrapper = Enzyme.mount(
+      <Pagination pageCount={11} currentPage={11} getPageHref={getPageHref} />
+    );
     expect(wrapper.find("a").last().hasClass("disabled")).to.equal(true);
     expect(
       wrapper.find("a").last().find("[aria-disabled='true']")
@@ -37,17 +45,15 @@ describe("Pagination Test", () => {
   });
 
   it("Current page item has active class", () => {
-    wrapper = Enzyme.mount(<Pagination pageCount={11} currentPage={5} />);
+    wrapper = Enzyme.mount(
+      <Pagination pageCount={11} currentPage={5} getPageHref={getPageHref} />
+    );
     expect(wrapper.find("a").at(5).hasClass("selected")).to.equal(true);
   });
 
   it("When page item is selected, runs the onPageChange callback", () => {
     wrapper = Enzyme.mount(
-      <Pagination
-        pageCount={11}
-        currentPage={5}
-        onPageChange={changeCallback}
-      />
+      <Pagination pageCount={11} currentPage={5} getPageHref={getPageHref} />
     );
     expect(wrapper.find("a").at(2).simulate("click"));
     expect(changeCallback.callCount).to.equal(1);
@@ -55,7 +61,7 @@ describe("Pagination Test", () => {
 
   it("When pagination has 1 element, one element is shown with both buttons disabled", () => {
     wrapper = Enzyme.mount(
-      <Pagination pageCount={1} currentPage={1} onPageChange={changeCallback} />
+      <Pagination pageCount={1} currentPage={1} getPageHref={getPageHref} />
     );
 
     // Previous/Next buttons + list = 3 total items
