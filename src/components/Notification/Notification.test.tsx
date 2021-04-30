ef4d25a280c3dfeb279306aeff224dc0454c7cf2
Serena Berry <serenaberry@nypl.org>
2021-04-30 12:01:09 -0400

    Make test suite pass. Update `CHANGELOG`

--- src/components/Notification/Notification.test.tsx
+++ src/components/Notification/Notification.test.tsx
@@ -9,20 +9,8 @@ describe("Notification", () => {
     <Notification id="notificationID">Notification content</Notification>
   );
 
-  const notificationWithNoContent = (
-    <Notification
-      id="notification#1"
-      className="edition-notification"
-    ></Notification>
-  );
-
   it("Generates a Notification", () => {
     const notification = Enzyme.mount(regularNotification);
-    expect(notification.find(".notification__content")).to.have.lengthOf(1);
-  });
-
-  it("Generates a notification without a content block if one isn't provided", () => {
-    const notification = Enzyme.mount(notificationWithNoContent);
-    expect(notification.find(".notification__content")).to.have.lengthOf(0);
+    expect(notification.find(".notification")).to.have.lengthOf(1);
   });
 });
