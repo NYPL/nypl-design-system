596ed0cdc874c19f8fc540a3d8e812adb0806332
Michael Kaplan <6432596+kaplanmichael@users.noreply.github.com>
2021-05-18 11:16:44 -0400

    [DSD-165] Add background color to mdx docs to improve visibility

--- tsd.d.ts
+++ tsd.d.ts
@@ -1,6 +1,7 @@
 declare module "*.svg" {
-  import React = require("react");
-  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: string;
+
+  export { ReactComponent };
+  export default content;
 }
