313fa1f1562eb12fbbd4146eb6e579c25a3336b2
bigfishdesign13 <marty@brutvan.com>
2021-06-28 15:44:26 -0400

    updated style guide details

--- src/components/Breakpoints/Breakpoints.stories.tsx
+++ src/components/Breakpoints/Breakpoints.stories.tsx
@@ -2,13 +2,7 @@ import * as React from "react";
 
 import cssVariables from "../../helpers/CSSVariablesHelper";
 import getCSSVariable from "../../helpers/getCSSVariable";
-import Heading from "../Heading/Heading";
 import { CSSVariablesInterface } from "../../interfaces";
-import { getCategory } from "../../utils/componentCategories";
-
-export default {
-  title: getCategory("Breakpoints"),
-};
 
 const breakpointVariables: CSSVariablesInterface = {};
 
@@ -25,38 +19,53 @@ const breakpointSizes = {
   Mobile: "small",
 };
 
-const breakpointDocs = [];
+const breakpointValuesList = [];
 
 for (const [key, value] of Object.entries(breakpointSizes)) {
-  breakpointDocs.push(
-    <div style={{ marginBottom: "2%" }}>
-      <div>
-        <span
-          style={{
-            marginRight: "var(--space-s)",
-          }}
-        >
-          {key}
-        </span>
-        <span>--breakpoint-{value}</span>
-      </div>
-      <p>{getCSSVariable(`--breakpoint-${value}`)}</p>
-    </div>
+  breakpointValuesList.push(
+    <li>
+      <span>{key}:</span>
+      <span>{getCSSVariable(`--breakpoint-${value}`)}</span>
+    </li>
+  );
+}
+
+const breakpointVarsList = [];
+
+for (const [key, value] of Object.entries(breakpointSizes)) {
+  breakpointVarsList.push(
+    <span className={`${key}`}>
+      --breakpoint-{value}: {getCSSVariable(`--breakpoint-${value}`)}
+      <br />
+    </span>
   );
 }
 
 export const breakpoints = () => (
   <>
-    <Heading level={1}>Breakpoints</Heading>
-    {breakpointDocs}
+    <ul>{breakpointValuesList}</ul>
+    <pre>{breakpointVarsList}</pre>
   </>
 );
 
 breakpoints.storyName = "Breakpoints";
-breakpoints.parameters = {
-  design: {
-    type: "figma",
-    url:
-      "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Master?node-id=17983%3A60146",
-  },
-};
+
+// const breakpointVarsList = [];
+
+// for (const [key, value] of Object.entries(breakpointSizes)) {
+//   breakpointVarsList.push(
+//     <div style={{ marginBottom: "2%" }}>
+//       <div>
+//         <span
+//           style={{
+//             marginRight: "var(--space-s)",
+//           }}
+//         >
+//           {key}
+//         </span>
+//         <span>--breakpoint-{value}</span>
+//       </div>
+//       <p>{getCSSVariable(`--breakpoint-${value}`)}</p>
+//     </div>
+//   );
+// }
