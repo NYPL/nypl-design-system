129561bcc200b934af78d48afdf66cf1c7a835d0
bigfishdesign13 <marty@brutvan.com>
2021-06-29 15:58:14 -0400

    another incremental update

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 
 import bem from "../../utils/bem";
-import { NotificationTypes } from "./NotificationTypes";
 import Button from "../Button/Button";
 import { ButtonTypes } from "../Button/ButtonTypes";
+import Heading from "../Heading/Heading";
 import Icon from "../Icons/Icon";
 import { IconNames } from "../Icons/IconTypes";
+import { NotificationTypes } from "./NotificationTypes";
 
 export interface NotificationProps {
-  /** ClassName that appears in addition to "notification" */
-  className?: string;
   /** BlockName for use with BEM. See how to work with blockNames and BEM here: http://getbem.com/introduction/ */
   blockName?: string;
-  /** Controls whether a Notification can be dismissed (closed) by a user */
+  /** Optional prop to control horizontal alignment of the `Notification` content */
+  centered?: boolean;
+  /** Optional `className` that appears in addition to "notification" */
+  className?: string;
+  /** Optional prop to control whether a `Notification` can be dismissed (closed) by a user */
   dismissible?: boolean;
-  /** Customize icon */
+  /** Optional custom `Icon` that will override the default `Icon` */
   icon?: React.ReactNode;
   /** ID that other components can cross reference for accessibility purposes */
   id?: string;
-  /** Controls the positioning of a Notification component */
+  /** Optional prop to control the padding around the `Notification` component */
   fillSpace?: boolean;
-  /** Controls the alignment and coloring of notification text and the visibility of an applicable icon */
+  /** Optional prop to control the coloring of notification text and the visibility of an applicable icon */
   notificationType?: NotificationTypes;
   /** Modifiers array for use with BEM. See how to work with modifiers and BEM here: http://getbem.com/introduction/ */
   modifiers?: string[];
+}
+
+// NotificationHeading child-component
+export function NotificationHeading({ ...props }) {
+  const { children, className } = props;
+  return (
+    <Heading
+      level={4}
+      className={bem("notification-heading", [], "", [className])}
+    >
+      {children}
+    </Heading>
+  );
+}
+
+// NotificationHeading child-component
+export function NotificationContent({ ...props }) {
+  const { children, className } = props;
+  return (
+    <div className={bem("notification-content", [], "", [className])}>
+      {children}
+    </div>
+  );
 }
 
 export default function Notification(
@@ -31,6 +57,7 @@ export default function Notification(
 ) {
   const {
     blockName,
+    centered,
     children,
     className,
     dismissible = false,
@@ -51,16 +78,11 @@ export default function Notification(
 
   const baseClass = "notification";
 
-  const styleProps = { style: {} };
-  // if (placement) {
-  //   styleProps.style["position"] = placement;
-
-  //   if (placement === "global") styleProps.style["top"] = 0;
-  // }
-
   const notificationModifiers = modifiers ? modifiers : [];
 
   notificationModifiers.push(notificationType);
+
+  if (centered) notificationModifiers.push("centered");
 
   if (fillSpace) notificationModifiers.push("fill-space");
 
@@ -91,17 +113,66 @@ export default function Notification(
     }
   };
 
-  if (!icon) modifiers.push("no-icon");
+  let childHeading,
+    childContent,
+    headingCount = 0,
+    contentCount = 0;
+
+  React.Children.map(children, function (child: React.ReactElement) {
+    if (child.type === NotificationHeading) {
+      childHeading = child;
+      headingCount++;
+    }
+
+    if (child.type === NotificationContent) {
+      childContent = child;
+      contentCount++;
+    }
+  });
+
+  if (headingCount > 1) {
+    console.error(
+      `Only one NotificationHeading child component may be passed to Notification.`
+    );
+    childHeading =
+      "Error: Only one NotificationHeading child component may be passed to Notification.";
+  }
+
+  if (contentCount > 1) {
+    console.error(
+      `Only one NotificationContent child component may be passed to Notification.`
+    );
+    childContent =
+      "Error: Only one NotificationContent child component may be passed to Notification.";
+  }
+
+  if (
+    headingCount &&
+    (icon ||
+      ((notificationType === NotificationTypes.Announcement ||
+        notificationType === NotificationTypes.Warning) &&
+        !centered))
+  )
+    modifiers.push("align-text");
 
   return (
     <aside
       className={bem(baseClass, notificationModifiers, blockName, [className])}
       id={id}
-      {...styleProps}
     >
       <div className={bem("container", [], baseClass)}>
-        {iconElement()}
-        {children}
+        {headingCount ? (
+          <header>
+            {iconElement()}
+            {childHeading}
+          </header>
+        ) : null}
+        {contentCount ? (
+          <div className="content">
+            {!headingCount ? iconElement() : null}
+            {childContent}
+          </div>
+        ) : null}
       </div>
       {dismissible ? (
         <Button
