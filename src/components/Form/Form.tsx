05faf625d7182913b5a3f5921a67b605967b735b
bigfishdesign13 <marty@brutvan.com>
2021-06-30 16:53:28 -0400

    incremental update

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,18 @@
-// HorizontalRule
 import * as React from "react";
 import bem from "../../utils/bem";
 import generateUUID from "../../helpers/generateUUID";
 
 export interface FormProps {
+  /** Optional form `action` attribute */
+  action?: string;
   /** Optional additional attributes passed to the `<form>` element */
   attributes?: { [key: string]: any };
   /** Optional className you can add in addition to `form` */
   className?: string;
   /** Optional ID that other components can cross reference */
   id?: string;
+  /** Optional form `method` attribute */
+  method?: "get" | "post";
   /** Optional modifiers array for use with BEM. See how to work with modifiers and BEM here: http://getbem.com/introduction/ */
   modifiers?: string[];
 }
@@ -29,9 +32,23 @@ export function FormField({ ...props }) {
 }
 
 export default function Form(props: React.ComponentProps<"form"> & FormProps) {
-  const { attributes = {}, children, className, id, modifiers = [] } = props;
+  const {
+    action,
+    attributes = {},
+    children,
+    className,
+    id,
+    method,
+    modifiers = [],
+  } = props;
 
   const formID = id || generateUUID();
+
+  action && (attributes["action"] = action);
+
+  method &&
+    (method === "get" || method === "post") &&
+    (attributes["method"] = method);
 
   return (
     <form
