5fc9c973ae19e8fb3a64f3989e1ff228d28b44d8
bigfishdesign13 <marty@brutvan.com>
2021-07-02 09:29:31 -0400

    added tests

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,51 +1,61 @@
-// import { expect } from "chai";
-// import * as Enzyme from "enzyme";
-// import * as React from "react";
+import { expect } from "chai";
+import * as Enzyme from "enzyme";
+import * as React from "react";
 
-// import Label from "./Label";
+import Form, { FormRow, FormField } from "./Form";
+import TextInput from "../TextInput/TextInput";
 
-// describe("Label Test", () => {
-//   let wrapper: Enzyme.ShallowWrapper<any, any>;
-//   it("Renders a <label> when passed text as children", () => {
-//     wrapper = Enzyme.shallow(
-//       <Label id="label" htmlFor="so-lonely">
-//         {"Cupcake's"}
-//       </Label>
-//     );
-//     expect(wrapper.find("label")).to.have.lengthOf(1);
-//   });
+describe("Form", () => {
+  //   let wrapper: Enzyme.ShallowWrapper<any, any>;
+  it("Renders a <form> element", () => {
+    const wrapper = Enzyme.shallow(<Form></Form>);
+    expect(wrapper.find("form")).to.have.lengthOf(1);
+  });
 
-//   it("Renders a <label> when passed element as children", () => {
-//     wrapper = Enzyme.shallow(
-//       <Label id="label" htmlFor="so-lonely">
-//         <span>Cupcakes</span>
-//       </Label>
-//     );
-//     expect(wrapper.find("label")).to.have.lengthOf(1);
-//   });
+  it("Renders a <form> element with child .form-row element", () => {
+    const wrapper = Enzyme.mount(
+      <Form>
+        <FormRow></FormRow>
+      </Form>
+    );
+    expect(wrapper.find(".form")).to.have.lengthOf(1);
+    expect(wrapper.find(".form .form-row")).to.have.lengthOf(1);
+  });
 
-//   it("Renders the optional or required helper text", () => {
-//     wrapper = Enzyme.shallow(
-//       <Label id="label" htmlFor="so-lonely">
-//         <span>Cupcakes</span>
-//       </Label>
-//     );
-//     expect(wrapper.find("div")).to.have.lengthOf(0);
+  it("Renders a <form> element with child .form-field element", () => {
+    const wrapper = Enzyme.mount(
+      <Form>
+        <FormField></FormField>
+      </Form>
+    );
+    expect(wrapper.find(".form")).to.have.lengthOf(1);
+    expect(wrapper.find(".form .form-field")).to.have.lengthOf(1);
+  });
 
-//     wrapper = Enzyme.shallow(
-//       <Label id="label" htmlFor="so-lonely" optReqFlag="Optional">
-//         <span>Cupcakes</span>
-//       </Label>
-//     );
-//     expect(wrapper.find("div")).to.have.lengthOf(1);
-//     expect(wrapper.find("div").text()).to.equal("Optional");
+  it("Renders a <form> element with .form-row, .form-field and input elements properly nested", () => {
+    const wrapper = Enzyme.mount(
+      <Form>
+        <FormRow>
+          <FormField>
+            <TextInput labelText="Input Field" />
+          </FormField>
+        </FormRow>
+      </Form>
+    );
+    expect(wrapper.find(".form")).to.have.lengthOf(1);
+    expect(wrapper.find(".form .form-row")).to.have.lengthOf(1);
+    expect(wrapper.find(".form .form-row .form-field")).to.have.lengthOf(1);
+    expect(
+      wrapper.find(".form .form-row .form-field .textinput")
+    ).to.have.lengthOf(1);
+  });
 
-//     wrapper = Enzyme.shallow(
-//       <Label id="label" htmlFor="so-lonely" optReqFlag="Required">
-//         <span>Cupcakes</span>
-//       </Label>
-//     );
-//     expect(wrapper.find("div")).to.have.lengthOf(1);
-//     expect(wrapper.find("div").text()).to.equal("Required");
-//   });
-// });
+  it("Renders a <form> element with .form-row and .form-field elements properly nested", () => {
+    const wrapper = Enzyme.shallow(
+      <Form action="/end/point" method="get"></Form>
+    );
+    expect(wrapper.find("form")).to.have.lengthOf(1);
+    expect(wrapper.find("form").prop("action")).to.equal("/end/point");
+    expect(wrapper.find("form").prop("method")).to.equal("get");
+  });
+});
