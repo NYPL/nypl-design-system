6fac25f8304d247e2a0ffc1658b17db7cfa01725
Edwin Guzman <edwin.gzmn@gmail.com>
2021-11-30 15:14:11 -0500

    Init commit with a few component stories being updated.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -27,3 +27,19 @@ export const getVariant = (variant, collection, fallback = null) => {
   }
   return variantMap[variant] || fallback;
 };
+
+/**
+ * Given an enum object and its name, this will return (1) an array `options`
+ * of all values with the enum name as the prefix, and (2) a function that
+ * returns the correct enum value given a string from the `options` array.
+ */
+export const getStorybookEnumValues = (enumObject, name) => {
+  const options = Object.keys(enumObject).map((key) => `${name}.${key}`);
+  const getValue = (key) => {
+    // In case no value is passed, return the first value from the array above
+    // as the default. Otherwise, remove the dot from the string and get the
+    // last part to be able to get the correct enum value.
+    return !key ? options[0] : enumObject[key.substr(key.indexOf(".") + 1)];
+  };
+  return { options, getValue };
+};
