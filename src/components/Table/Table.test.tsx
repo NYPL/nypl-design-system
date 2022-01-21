e9bffa23d60d43b578153c0aa303886d7bae1790
Deepika Gonuguntla <consultdg@nypl.org>
2022-01-21 09:07:33 -0500

    Added test cases

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -1,6 +1,64 @@
 import * as React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
-import renderer from "react-test-renderer";
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption,} from '@chakra-ui/react'
 
-import Table from "./Table";
+describe("Table Accessibility", () => {
+  it("passes axe accessibility test with children components", async () => {
+    const { container } = render(
+      <Table>
+      	<TableCaption></TableCaption>
+        <Thead>
+          <Tr>
+            <Th>First Name</Th>
+            <Th>Last Name</Th>
+            <Th>Nick Name</Th>
+            <Th>Address1</Th>
+            <Th>City</Th>
+            <Th>Zipcode</Th>
+            <Th>State</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Tom</Td>
+            <Td>Nook</Td>
+            <Td>Tanukichi</Td>
+            <Td>Main Street</Td>
+            <Td>New York</Td>
+            <Td>23458</Td>
+            <Td>NY</Td>
+          </Tr>
+          <Tr>
+            <Td>Isabelle</Td>
+            <Td>-</Td>
+            <Td>Shizue</Td>
+            <Td>Walnut Street</Td>
+            <Td>New York</Td>
+            <Td>23458</Td>
+            <Td>NY</Td>
+          </Tr>
+          <Tr>
+            <Td>K.K.</Td>
+            <Td>Slider</Td>
+            <Td>Totakeke</Td>
+            <Td>Niper Place</Td>
+            <Td>New York</Td>
+            <Td>98765</Td>
+            <Td>NY</Td>
+          </Tr>
+          <Tr>
+            <Td>Sonny</Td>
+            <Td>Resetti</Td>
+            <Td>Risetto san</Td>
+            <Td>Village Road</Td>
+            <Td>New York</Td>
+            <Td>09873</Td>
+            <Td>NY</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
