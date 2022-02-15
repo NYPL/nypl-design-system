e61745dbb618c4cd63509f81821f88c9524d38a9
Edwin Guzman <edwin.gzmn@gmail.com>
2022-02-15 16:41:59 -0500

    Fixing Table issue with white text color on a white background for row headers

--- src/theme/components/customTable.ts
+++ src/theme/components/customTable.ts
@@ -1,8 +1,28 @@
+// What we might expect users to pass for the color "white". This is
+// temporary until we have a better color contrast checker.
+const whiteColors = [
+  "#fff",
+  "#ffffff",
+  "#FFF",
+  "#FFFFFF",
+  "white",
+  "var(--nypl-colors-ui-white)",
+];
 const baseStyle = ({
   columnHeadersTextColor,
   showRowDividers,
   useRowHeaders,
 }) => ({
+  // Just so we don't let row headers to display as white. Only the
+  // thead should have white text color with any color background.
+  tbody: {
+    th: {
+      color:
+        useRowHeaders && whiteColors.includes(columnHeadersTextColor)
+          ? "var(--nypl-colors-ui-black)"
+          : columnHeadersTextColor,
+    },
+  },
   th: {
     border: showRowDividers ? undefined : "none",
     borderBottom: showRowDividers ? undefined : "0px",
