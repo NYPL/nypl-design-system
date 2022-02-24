f13f646825fb019e39e4272f42ab887bef0a2112
Marty Brutvan <marty@brutvan.com>
2022-02-24 09:39:35 -0500

    incremental update with some new logos

--- src/components/Logo/LogoSvgs.tsx
+++ src/components/Logo/LogoSvgs.tsx
@@ -1,33 +1,53 @@
 /* eslint-disable camelcase */
 import logo_bpl_black from "../../../icons/svg/logo_bpl_black.svg";
+import logo_bpl_white from "../../../icons/svg/logo_bpl_white.svg";
 import logo_lpa_color from "../../../icons/svg/logo_lpa_color.svg";
+import logo_lpa_black from "../../../icons/svg/logo_lpa_black.svg";
 import logo_lpa_white from "../../../icons/svg/logo_lpa_white.svg";
 import logo_nypl_full_black from "../../../icons/svg/logo_nypl_full_black.svg";
 import logo_nypl_full_white from "../../../icons/svg/logo_nypl_full_white.svg";
 import logo_nypl_lion_black from "../../../icons/svg/logo_nypl_lion_black.svg";
 import logo_nypl_lion_white from "../../../icons/svg/logo_nypl_lion_white.svg";
 import logo_qpl_alt_black from "../../../icons/svg/logo_qpl_alt_black.svg";
+import logo_qpl_alt_white from "../../../icons/svg/logo_qpl_alt_white.svg";
+import logo_qpl_black from "../../../icons/svg/logo_qpl_black.svg";
 import logo_qpl_color from "../../../icons/svg/logo_qpl_color.svg";
+import logo_qpl_white from "../../../icons/svg/logo_qpl_white.svg";
+import logo_schomburg_black from "../../../icons/svg/logo_schomburg_black.svg";
+import logo_schomburg_circle_black from "../../../icons/svg/logo_schomburg_circle_black.svg";
 import logo_schomburg_circle_color from "../../../icons/svg/logo_schomburg_circle_color.svg";
+import logo_schomburg_circle_white from "../../../icons/svg/logo_schomburg_circle_white.svg";
 import logo_schomburg_color from "../../../icons/svg/logo_schomburg_color.svg";
+import logo_schomburg_white from "../../../icons/svg/logo_schomburg_white.svg";
 import logo_simplye_black from "../../../icons/svg/logo_simplye_black.svg";
+import logo_simplye_white from "../../../icons/svg/logo_simplye_white.svg";
 import logo_simplye_color from "../../../icons/svg/logo_simplye_color.svg";
 import logo_snfl_black from "../../../icons/svg/logo_snfl_black.svg";
 import logo_snfl_white from "../../../icons/svg/logo_snfl_white.svg";
 
 export default {
   logo_bpl_black,
+  logo_bpl_white,
+  logo_lpa_black,
   logo_lpa_color,
   logo_lpa_white,
   logo_nypl_full_black,
   logo_nypl_full_white,
-  logo_nypl_lion_white,
   logo_nypl_lion_black,
+  logo_nypl_lion_white,
   logo_qpl_alt_black,
+  logo_qpl_alt_white,
+  logo_qpl_black,
   logo_qpl_color,
+  logo_qpl_white,
+  logo_schomburg_black,
+  logo_schomburg_circle_black,
   logo_schomburg_circle_color,
+  logo_schomburg_circle_white,
   logo_schomburg_color,
+  logo_schomburg_white,
   logo_simplye_black,
+  logo_simplye_white,
   logo_simplye_color,
   logo_snfl_black,
   logo_snfl_white,
