f4cd93c639ea9542a5d453c2c6ed10badca2847f
Edwin Guzman <edwin.gzmn@gmail.com>
2022-04-21 13:32:27 -0400

    Updating the modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import {
+  chakra,
   Modal as ChakraModal,
   ModalOverlay,
   ModalContent,
@@ -13,95 +14,87 @@ import * as React from "react";
 
 import Button from "../Button/Button";
 
-export interface ModalProps {
+interface BaseModalProps {
   bodyContent?: string;
-  buttonText?: string;
   closeButtonLabel?: string;
   headingText?: string;
-  /** ID that other components can cross reference for accessibility purposes */
-  id?: string;
   onCloseHandler?: () => void;
 }
 
-const testBodyContent = `Lorem ipsum dolor sit amet, consectetur adipiscing
-  elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  Tempus quam pellentesque nec nam. Sit amet purus gravida quis blandit
-  turpis cursus. Est ante in nibh mauris. Ut faucibus pulvinar
-  elementum integer. Mattis ullamcorper velit sed ullamcorper morbi
-  tincidunt ornare massa. Consectetur purus ut faucibus pulvinar
-  elementum integer enim. Accumsan sit amet nulla facilisi morbi.`;
-
-export function ModalTrigger({
-  bodyContent = testBodyContent,
-  buttonText,
-  closeButtonLabel = "Close",
-  headingText,
-  id,
-  onCloseHandler,
-}: React.PropsWithChildren<ModalProps>) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const finalOnCloseHandler = () => {
-    onCloseHandler && onCloseHandler();
-    onClose();
-  };
-  return (
-    <>
-      <Button onClick={onOpen}>{buttonText}</Button>
-
-      <ChakraModal isOpen={isOpen} onClose={onClose} id={id}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{headingText}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>{bodyContent}</ModalBody>
-
-          <ModalFooter>
-            <ButtonGroup>
-              <Button onClick={finalOnCloseHandler}>{closeButtonLabel}</Button>
-              <Button onClick={() => alert("another action")}>
-                Secondary Action
-              </Button>
-            </ButtonGroup>
-          </ModalFooter>
-        </ModalContent>
-      </ChakraModal>
-    </>
-  );
+export interface ModalProps {
+  buttonText?: string;
+  /** ID that other components can cross reference for accessibility purposes */
+  id?: string;
+  modalProps: BaseModalProps;
 }
 
-export function useModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  function Modal({
-    bodyContent = testBodyContent,
-    closeButtonLabel = "Close",
-    headingText,
+export const ModalTrigger = chakra(
+  ({
+    buttonText,
     id,
-    onCloseHandler,
-  }: React.PropsWithChildren<ModalProps>) {
+    modalProps,
+    ...rest
+  }: React.PropsWithChildren<ModalProps>) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const finalOnCloseHandler = () => {
-      onCloseHandler && onCloseHandler();
+      modalProps.onCloseHandler && modalProps.onCloseHandler();
       onClose();
     };
     return (
-      <ChakraModal isOpen={isOpen} onClose={onClose} id={id}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{headingText}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>{bodyContent}</ModalBody>
+      <>
+        <Button id="modal-open-btn" onClick={onOpen}>
+          {buttonText}
+        </Button>
 
-          <ModalFooter>
-            <ButtonGroup>
-              <Button onClick={finalOnCloseHandler}>{closeButtonLabel}</Button>
-              <Button onClick={() => alert("another action")}>
-                Secondary Action
-              </Button>
-            </ButtonGroup>
-          </ModalFooter>
-        </ModalContent>
-      </ChakraModal>
+        <ChakraModal isOpen={isOpen} onClose={onClose} id={id} {...rest}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{modalProps.headingText}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>{modalProps.bodyContent}</ModalBody>
+
+            <ModalFooter>
+              <ButtonGroup>
+                <Button id="modal-close-btn" onClick={finalOnCloseHandler}>
+                  {modalProps.closeButtonLabel || "Close"}
+                </Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </ModalContent>
+        </ChakraModal>
+      </>
     );
   }
+);
+
+export function useModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const Modal = chakra(
+    ({ id, modalProps, ...rest }: React.PropsWithChildren<ModalProps>) => {
+      const finalOnCloseHandler = () => {
+        modalProps.onCloseHandler && modalProps.onCloseHandler();
+        onClose();
+      };
+      return (
+        <ChakraModal isOpen={isOpen} onClose={onClose} id={id} {...rest}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>{modalProps.headingText}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>{modalProps.bodyContent}</ModalBody>
+
+            <ModalFooter>
+              <ButtonGroup>
+                <Button id="modal-close-btn" onClick={finalOnCloseHandler}>
+                  {modalProps.closeButtonLabel || "Close"}
+                </Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </ModalContent>
+        </ChakraModal>
+      );
+    }
+  );
 
   return { onOpen, Modal };
 }
