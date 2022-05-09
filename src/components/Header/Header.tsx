13956677a40b2c8d8f0c85cc0f8ffa7e2ba46aa2
Olivia Wong <oliviawong@nypl.org>
2022-05-09 16:00:14 -0400

    More updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -144,7 +144,7 @@ export const Header = chakra(() => {
             <Logo
               __css={{ ...styles.logo, marginTop: "13px", height: "30px" }}
               id="header-nypl-logo"
-              name="logo_nypl_full_black"
+              name="nyplFullBlack"
               size="xxsmall"
               title="NYPL Header Logo"
             />
@@ -153,20 +153,20 @@ export const Header = chakra(() => {
           <HStack __css={styles.icons}>
             <Icon
               id="log-in-icon"
-              name="utility_account_unfilled"
+              name="utilityAccountUnfilled"
               size="medium"
               title="Log in to your account"
             />
             <Icon
               id="locator-icon"
-              name="action_help_outline"
+              name="actionHelpOutline"
               size="medium"
               title="NYPL Locator"
             />
             <Icon id="search-icon" name="search" size="medium" title="Search" />
             <Icon
               id="hamburger-icon"
-              name="utility_hamburger"
+              name="utilityHamburger"
               size="medium"
               title="Menu"
             />
