c054be6e290e95a6b639ef1ee144200db6112e74
Edwin Guzman <edwin.gzmn@gmail.com>
2022-06-08 13:59:32 -0400

    Organizing scss files and importing Datepicker css into the component

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -2,6 +2,8 @@ import { chakra, useMultiStyleConfig } from "@chakra-ui/react";
 import React, { useState, forwardRef } from "react";
 import ReactDatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+
 import Fieldset from "../Fieldset/Fieldset";
 import { FormRow, FormField } from "../Form/Form";
 import HelperErrorText, {
