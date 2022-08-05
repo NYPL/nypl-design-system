dd20688a6525930662e3c807783f4f3e936dc47c
Edwin Guzman <edwin.gzmn@gmail.com>
2022-08-05 12:25:01 -0400

    Adding TemplateContentBottom and updating the DOM structure of the Template components

--- src/theme/components/template.ts
+++ src/theme/components/template.ts
@@ -2,13 +2,6 @@
  * Grid layout based on https://www.joshwcomeau.com/css/full-bleed/
  */
 
-const breakout = {
-  width: "100%",
-  // This could be "1 / 4" and it would mean the same. This is
-  // "future-proof" the grid column assignment to the last column.
-  gridColumn: "1 / -1",
-};
-
 const Template = {
   baseStyle: {
     boxSizing: "border-box",
@@ -18,10 +11,6 @@ const Template = {
       1fr
       min(1280px, 100%)
       1fr`,
-    // Set all elements to start on the second 1280px grid column.
-    "> *": {
-      gridColumn: "2",
-    },
     rowGap: "grid.l",
   },
   sizes: {},
@@ -30,10 +19,18 @@ const Template = {
 // Elements that need to breakout will span outside
 // the center 1280px grid column.
 const TemplateBreakout = {
-  baseStyle: breakout,
+  baseStyle: {
+    width: "100%",
+    // This could be "1 / 4" and it would mean the same. This is
+    // "future-proof" the grid column assignment to the last column.
+    gridColumn: "1 / -1",
+  },
 };
 const TemplateContent = {
   baseStyle: {
+    // Set this element to start on the second 1280px grid column.
+    gridColumn: "2",
+    // But this element also contains its own grid system within.
     display: "grid",
     gridTemplateColumns: "1fr",
     paddingY: 0,
@@ -51,7 +48,7 @@ const TemplateContent = {
     },
   },
 };
-const TemplateContentTop = {
+const TemplateContentTopBottom = {
   baseStyle: {
     gridColumn: { base: "1", md: "1 / span 2" },
     height: "100%",
@@ -81,7 +78,7 @@ export default {
   Template,
   TemplateBreakout,
   TemplateContent,
-  TemplateContentTop,
+  TemplateContentTopBottom,
   TemplateContentPrimary,
   TemplateContentSidebar,
 };
