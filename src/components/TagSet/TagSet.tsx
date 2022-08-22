be0887da34854b9602dfdba3e77743d0286bb659
Edwin Guzman <edwin.gzmn@gmail.com>
2022-08-22 11:00:35 -0400

    Minor updates

--- src/components/TagSet/TagSet.tsx
+++ src/components/TagSet/TagSet.tsx
@@ -11,24 +11,33 @@ import TagSetFilter, {
 } from "./TagSetFilter";
 
 export interface BaseTagSetProps {
-  /** Additional class for the component */
+  /** Additional class for the componen.t */
   className?: string;
-  /** ID that other components can cross reference for accessibility purposes */
+  /** ID that other components can cross reference for accessibility purposes. */
   id?: string;
 }
+// We want either the "explore" or "filter" type props.
 export type TagSetTypeProps = TagSetFilterProps | TagSetExploreProps;
+// And here combine the special types with the base props.
 export type TagSetProps = BaseTagSetProps & TagSetTypeProps;
 
+// Type guard so we can make sure we have a "filter" `TagSet` variant.
 export function isFilterType(type: TagSetProps["type"]): type is "filter" {
   return type === "filter";
 }
 
 /**
- * The `TagSet` component
+ * The `TagSet` component renders a group of individual tags which have two
+ * variants: "explore" and "filter". The "explore" tags are meant to be used for
+ * exploratory linkable elements. Whereas the "filter" tags are used to display
+ * the filter values that were selected through another UI. Only "filter" tags
+ * can be dismissible.
  */
 export const TagSet = chakra<As<TagSetProps>, TagSetProps>(
   forwardRef<HTMLDivElement, TagSetProps>((props: TagSetProps, ref?) => {
     const {
+      className,
+      id,
       isDismissible = false,
       onClick,
       tagSetData = [],
@@ -36,6 +45,19 @@ export const TagSet = chakra<As<TagSetProps>, TagSetProps>(
       ...rest
     } = props;
     const styles = useStyleConfig("TagSet", {});
+
+    // if isDissible and type="explore":
+    // NYPL Reservoir TagSet: The `isDismissible` prop will be ignored
+    // when the `type` prop is set to "explore."
+
+    // if onclick and filter:
+    // NYPL Reservoir TagSet: The `onClick` prop will be ignored when the
+    // `type` prop is set to "explore."
+
+    // if explore and labels are strings:
+    // NYPL Reservoir TagSet: Explore tags require all `label` props to be strings.
+    // if filter and labels are jsx:
+    // NYPL Reservoir TagSet: Explore tags require all `label` props to be strings.
 
     return (
       <Flex ref={ref} __css={styles} {...rest}>
@@ -57,25 +79,5 @@ export const TagSet = chakra<As<TagSetProps>, TagSetProps>(
     );
   })
 );
-
-// export const TestTag = () => {
-//   return (
-//     <TagSet
-//       isDismissible
-//       tagSetData={[
-//         {
-//           iconName: "search",
-//           label: "Search",
-//         },
-//         {
-//           iconName: "search",
-//           label: "Search",
-//         },
-//       ]}
-//       type="filter"
-//       onClick={(tagLabel: string) => console.log(tagLabel)}
-//     />
-//   );
-// };
 
 export default TagSet;
