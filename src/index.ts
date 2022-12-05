5b01100cff194aa218dc43eaf190148a112b943f
Edwin Guzman <edwin.gzmn@gmail.com>
2022-12-05 11:06:15 -0500

    Exporting feedbackbox

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,10 @@ export {
   FullDateType,
 } from "./components/DatePicker/DatePicker";
 export { default as DSProvider } from "./theme/provider";
+export {
+  default as FeedbackBox,
+  useFeedbackBox,
+} from "./components/FeedbackBox/FeedbackBox";
 export { default as Fieldset } from "./components/Fieldset/Fieldset";
 export { default as Footer } from "./components/Footer/Footer";
 export { default as Form, FormField, FormRow } from "./components/Form/Form";
