07561b50ac8f7cde6548d9b932c4a96123ae014e
bigfishdesign13 <marty@brutvan.com>
2023-04-28 16:50:11 -0400

    Storybook 7 conversion

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -41,6 +41,8 @@ const config: StorybookConfig = {
   stories: [
     "../src/components/Accordion/Accordion.stories.tsx",
     "../src/components/Accordion/Accordion.mdx",
+    "../src/components/HorizontalRule/HorizontalRule.stories.tsx",
+    "../src/components/HorizontalRule/HorizontalRule.mdx",
     "../src/docs/Welcome.mdx",
   ],
   typescript: {
---
+++ src/components/HorizontalRule/HorizontalRule.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+
+import HorizontalRule from "./HorizontalRule";
+
+const meta: Meta<typeof HorizontalRule> = {
+  title: "Components/Page Layout/HorizontalRule",
+  component: HorizontalRule,
+  decorators: [withDesign],
+  argTypes: {
+    className: { control: false },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof HorizontalRule>;
+
+/**
+ * Main Story for the HorizontalRule component. This must contains the `args`
+ *  and `parameters` properties in this object.
+ */
+export const WithControls: Story = {
+  args: {},
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=36818-25303&t=R7gWIzMTzUAGN7Sw-4",
+    },
+    jest: "HorizontalRule.test.tsx",
+  },
+};
