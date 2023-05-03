21385b4935d02fb37716546c0a83b50d97578fc2
Isa Stettler <stettlerisabelle@gmail.com>
2023-05-03 16:46:46 -0400

    update formatting

--- src/components/StatusBadge/StatusBadge.stories.tsx
+++ src/components/StatusBadge/StatusBadge.stories.tsx
@@ -5,17 +5,18 @@ import { withDesign } from "storybook-addon-designs";
 import StatusBadge, { statusBadgeTypeArray } from "./StatusBadge";
 
 const meta: Meta<typeof StatusBadge> = {
-    title: "Components/Page Layout/StatusBadge",
-    component: StatusBadge,
-    decorators: [withDesign],
-    argTypes: {
-        className: { control: false },
-        id: { control: false },
-        level: {
-            table: { defaultValue: { summary: "low" } },
-            control: { type: "select", }, options: statusBadgeTypeArray
-        },
+  title: "Components/Page Layout/StatusBadge",
+  component: StatusBadge,
+  decorators: [withDesign],
+  argTypes: {
+    className: { control: false },
+    id: { control: false },
+    level: {
+      table: { defaultValue: { summary: "low" } },
+      control: { type: "select" },
+      options: statusBadgeTypeArray,
     },
+  },
 };
 
 export default meta;
@@ -26,37 +27,38 @@ type Story = StoryObj<typeof StatusBadge>;
  *  and `parameters` properties in this object.
  */
 export const WithControls: Story = {
-    args: {
-        className: undefined,
-        id: "statusBadge-id",
-        level: "low",
+  args: {
+    className: undefined,
+    id: "statusBadge-id",
+    level: "low",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11256%3A0",
     },
-    parameters: {
-        design: {
-            type: "figma",
-            url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11256%3A0",
-        },
-        jest: "StatusBadge.test.tsx",
-    },
-    render: ({ ...args }) => {
-        return (<StatusBadge {...args}>Registration Required</StatusBadge>);
-
-    }
+    jest: "StatusBadge.test.tsx",
+  },
+  render: ({ ...args }) => {
+    return <StatusBadge {...args}>Registration Required</StatusBadge>;
+  },
 };
 
 // The following are additional StatusBadge example Stories.
 export const HigherBarrier: Story = {
-    render: () =>
-        <VStack spacing="s" align="stretch">
-            <StatusBadge level="high">On-Site Access Only</StatusBadge>
-            <StatusBadge level="medium">Cancelled</StatusBadge>
-        </VStack>
+  render: () => (
+    <VStack spacing="s" align="stretch">
+      <StatusBadge level="high">On-Site Access Only</StatusBadge>
+      <StatusBadge level="medium">Cancelled</StatusBadge>
+    </VStack>
+  ),
 };
 
 export const LowerBarrier: Story = {
-    render: () =>
-        <VStack spacing="s" align="stretch">
-            <StatusBadge level="low">Library Card Required</StatusBadge>
-            <StatusBadge level="low">Registration Required</StatusBadge>
-        </VStack>,
+  render: () => (
+    <VStack spacing="s" align="stretch">
+      <StatusBadge level="low">Library Card Required</StatusBadge>
+      <StatusBadge level="low">Registration Required</StatusBadge>
+    </VStack>
+  ),
 };
