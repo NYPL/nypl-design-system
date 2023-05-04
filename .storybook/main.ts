80266dccc4fbfb1e6762ee5e4a7aba4054f89b2a
Edwin Guzman <edwin.gzmn@gmail.com>
2023-05-04 16:47:29 -0400

    Updating Pagination documentation to Storybook 7 and fixing href bug

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -51,6 +51,8 @@ const config: StorybookConfig = {
     "../src/components/HorizontalRule/HorizontalRule.mdx",
     "../src/components/StatusBadge/StatusBadge.stories.tsx",
     "../src/components/StatusBadge/StatusBadge.mdx",
+    "../src/components/Pagination/Pagination.stories.tsx",
+    "../src/components/Pagination/Pagination.mdx",
     "../src/docs/Welcome.mdx",
   ],
   typescript: {
