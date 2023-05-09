4e11dc09f7a695b6af3698d1aef02dc79223c7a7
Isa Stettler <stettlerisabelle@gmail.com>
2023-05-09 14:44:23 -0400

    fix formatting

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -6,12 +6,12 @@ import { withDesign } from "storybook-addon-designs";
 import Checkbox from "./Checkbox";
 
 const meta: Meta<typeof Checkbox> = {
-    title: "Components/Form Elements/Checkbox",
-    component: Checkbox,
-    decorators: [withDesign],
-    argTypes: {
-        id: { control: false },
-    },
+  title: "Components/Form Elements/Checkbox",
+  component: Checkbox,
+  decorators: [withDesign],
+  argTypes: {
+    id: { control: false },
+  },
 };
 
 export default meta;
@@ -22,97 +22,112 @@ type Story = StoryObj<typeof Checkbox>;
  * and `parameters` properties in this object.
  */
 export const WithControls: Story = {
-    args: {
-        className: undefined,
-        helperText: "This is the helper text!",
-        id: "checkbox_id",
-        invalidText: "This is the error text :(",
-        isChecked: undefined,
-        isDisabled: false,
-        isIndeterminate: false,
-        isInvalid: false,
-        isRequired: false,
-        labelText: "Test Label",
-        name: "test_name",
-        onChange: undefined,
-        showHelperInvalidText: true,
-        showLabel: true,
-        value: "1",
+  args: {
+    className: undefined,
+    helperText: "This is the helper text!",
+    id: "checkbox_id",
+    invalidText: "This is the error text :(",
+    isChecked: undefined,
+    isDisabled: false,
+    isIndeterminate: false,
+    isInvalid: false,
+    isRequired: false,
+    labelText: "Test Label",
+    name: "test_name",
+    onChange: undefined,
+    showHelperInvalidText: true,
+    showLabel: true,
+    value: "1",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11895%3A658",
     },
-    parameters: {
-        design: {
-            type: "figma",
-            url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11895%3A658",
-        },
-        jest: ["Checkbox.test.tsx"],
-    },
+    jest: ["Checkbox.test.tsx"],
+  },
 };
 
 // The following are additional Checkbox example Stories.
 export const Checked: Story = {
-    render: () => <Checkbox id="isChecked" labelText="I am checked" isChecked value="1" />
+  render: () => (
+    <Checkbox id="isChecked" labelText="I am checked" isChecked value="1" />
+  ),
 };
 
 export const Indeterminate: Story = {
-    render: () => <Checkbox id="indeterminate" labelText="I am indeterminate" isIndeterminate />
+  render: () => (
+    <Checkbox
+      id="indeterminate"
+      labelText="I am indeterminate"
+      isIndeterminate
+    />
+  ),
 };
 
 export const AdditionalStates: Story = {
-    render: () =>
-        <VStack
-            align="left"
-            spacing="l"
-        >
-            <Box>
-                <Heading level="three">Focused</Heading>
-                <Checkbox id="focused" labelText="Click or tab to the Checkbox to see its focus state" />
-            </Box>
-            <Box>
-                <Heading level="three">Errored</Heading>
-                <HStack spacing="l">
-                    <Checkbox id="invalid" isInvalid labelText="I am in an error state" />
-                    <Checkbox
-                        id="invalid-checked"
-                        isInvalid
-                        isChecked
-                        labelText="I am checked in an error state"
-                    />
-                </HStack>
-            </Box>
-            <Box>
-                <Heading level="three">Disabled</Heading>
-                <HStack spacing="l">
-                    <Checkbox id="disabled" isDisabled labelText="I am disabled" />
-                    <Checkbox
-                        id="disabled-checked"
-                        isDisabled
-                        isChecked
-                        labelText="I am checked and disabled"
-                    />
-                </HStack>
-            </Box>
-            <Box>
-                <Heading level="three">With Helper Text</Heading>
-                <Checkbox
-                    id="helpertext"
-                    name="testHelperText"
-                    labelText="I have helper text"
-                    helperText="I am the helper text for this Checkbox"
-                />
-            </Box>
-            <Box>
-                <Heading level="three">With Errored Text</Heading>
-                <Checkbox
-                    id="invalid-text"
-                    invalidText="I am the error text for this Checkbox"
-                    isInvalid
-                    name="testinvalidText"
-                    labelText="I have error text"
-                />
-            </Box>
-        </VStack>
+  render: () => (
+    <VStack align="left" spacing="l">
+      <Box>
+        <Heading level="three">Focused</Heading>
+        <Checkbox
+          id="focused"
+          labelText="Click or tab to the Checkbox to see its focus state"
+        />
+      </Box>
+      <Box>
+        <Heading level="three">Errored</Heading>
+        <HStack spacing="l">
+          <Checkbox id="invalid" isInvalid labelText="I am in an error state" />
+          <Checkbox
+            id="invalid-checked"
+            isInvalid
+            isChecked
+            labelText="I am checked in an error state"
+          />
+        </HStack>
+      </Box>
+      <Box>
+        <Heading level="three">Disabled</Heading>
+        <HStack spacing="l">
+          <Checkbox id="disabled" isDisabled labelText="I am disabled" />
+          <Checkbox
+            id="disabled-checked"
+            isDisabled
+            isChecked
+            labelText="I am checked and disabled"
+          />
+        </HStack>
+      </Box>
+      <Box>
+        <Heading level="three">With Helper Text</Heading>
+        <Checkbox
+          id="helpertext"
+          name="testHelperText"
+          labelText="I have helper text"
+          helperText="I am the helper text for this Checkbox"
+        />
+      </Box>
+      <Box>
+        <Heading level="three">With Errored Text</Heading>
+        <Checkbox
+          id="invalid-text"
+          invalidText="I am the error text for this Checkbox"
+          isInvalid
+          name="testinvalidText"
+          labelText="I have error text"
+        />
+      </Box>
+    </VStack>
+  ),
 };
 
 export const WithJSXElementLabel: Story = {
-    render: () => <Checkbox id="jsx-label" labelText={<span>Arts</span>} name="jsxElementLabel" />
-};;
+  render: () => (
+    <Checkbox
+      id="jsx-label"
+      labelText={<span>Arts</span>}
+      name="jsxElementLabel"
+    />
+  ),
+};
--- src/components/CheckboxGroup/CheckboxGroup.stories.tsx
+++ src/components/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -10,292 +10,303 @@ import CheckboxGroup from "./CheckboxGroup";
 import { layoutTypesArray } from "../../helpers/types";
 
 const meta: Meta<typeof CheckboxGroup> = {
-    title: "Components/Form Elements/CheckboxGroup",
-    component: CheckboxGroup,
-    decorators: [withDesign],
-    argTypes: {
-        children: { table: { disable: true } },
-        defaultValue: { control: false },
-        id: { control: false },
-        isDisabled: {
-            table: { defaultValue: { summary: false } },
-        },
-        isFullWidth: {
-            table: { defaultValue: { summary: true } },
-        },
-        isInvalid: {
-            table: { defaultValue: { summary: false } },
-        },
-        isRequired: {
-            table: { defaultValue: { summary: false } },
-        },
-        key: { table: { disable: true } },
-        layout: {
-            table: { defaultValue: { summary: "column" } },
-            options: layoutTypesArray
-        },
-        name: { control: false },
-        onChange: { control: false },
-        ref: { table: { disable: true } },
-        showHelperInvalidText: {
-            table: { defaultValue: { summary: true } },
-        },
-        showLabel: {
-            table: { defaultValue: { summary: true } },
-        },
-        showRequiredLabel: {
-            table: { defaultValue: { summary: true } },
-        },
-        value: { control: false },
+  title: "Components/Form Elements/CheckboxGroup",
+  component: CheckboxGroup,
+  decorators: [withDesign],
+  argTypes: {
+    children: { table: { disable: true } },
+    defaultValue: { control: false },
+    id: { control: false },
+    isDisabled: {
+      table: { defaultValue: { summary: false } },
     },
-    parameters: {
-        design: {
-            type: "figma",
-            url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11895%3A658",
-        },
-        jest: ["CheckboxGroup.test.tsx"],
-    }
+    isFullWidth: {
+      table: { defaultValue: { summary: true } },
+    },
+    isInvalid: {
+      table: { defaultValue: { summary: false } },
+    },
+    isRequired: {
+      table: { defaultValue: { summary: false } },
+    },
+    key: { table: { disable: true } },
+    layout: {
+      table: { defaultValue: { summary: "column" } },
+      options: layoutTypesArray,
+    },
+    name: { control: false },
+    onChange: { control: false },
+    ref: { table: { disable: true } },
+    showHelperInvalidText: {
+      table: { defaultValue: { summary: true } },
+    },
+    showLabel: {
+      table: { defaultValue: { summary: true } },
+    },
+    showRequiredLabel: {
+      table: { defaultValue: { summary: true } },
+    },
+    value: { control: false },
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/qShodlfNCJHb8n03IFyApM/Main?node-id=11895%3A658",
+    },
+    jest: ["CheckboxGroup.test.tsx"],
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof CheckboxGroup>;
 
 export const WithControls: Story = {
-    args: {
-        defaultValue: ["4"],
-        helperText: "This is the helper text for the full group.",
-        id: "checkboxGroup-id",
-        invalidText: "This is the error text :(",
-        isDisabled: false,
-        isFullWidth: false,
-        isInvalid: false,
-        isRequired: false,
-        labelText: "Standard Checkbox Group",
-        layout: "column",
-        name: "checkbox-story",
-        onChange: undefined,
-        showHelperInvalidText: true,
-        showLabel: true,
-        showRequiredLabel: true,
-    },
-    render: (args) => (
-        <CheckboxGroup {...args}>
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
-    )
+  args: {
+    defaultValue: ["4"],
+    helperText: "This is the helper text for the full group.",
+    id: "checkboxGroup-id",
+    invalidText: "This is the error text :(",
+    isDisabled: false,
+    isFullWidth: false,
+    isInvalid: false,
+    isRequired: false,
+    labelText: "Standard Checkbox Group",
+    layout: "column",
+    name: "checkbox-story",
+    onChange: undefined,
+    showHelperInvalidText: true,
+    showLabel: true,
+    showRequiredLabel: true,
+  },
+  render: (args) => (
+    <CheckboxGroup {...args}>
+      <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+      <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+      <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+      <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+    </CheckboxGroup>
+  ),
 };
 
 // The following are additional Checkbox example Stories.
 
 export const Layout: Story = {
-    render: () => <VStack align="left"
-        spacing="l">
-        <CheckboxGroup
-            id="column"
-            labelText="Column (default)"
-            name="column-example"
-        >
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
-        <br />
-        <CheckboxGroup id="row" labelText="Row" name="row-example" layout="row">
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
+  render: () => (
+    <VStack align="left" spacing="l">
+      <CheckboxGroup
+        id="column"
+        labelText="Column (default)"
+        name="column-example"
+      >
+        <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+        <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+        <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+        <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+      </CheckboxGroup>
+      <br />
+      <CheckboxGroup id="row" labelText="Row" name="row-example" layout="row">
+        <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+        <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+        <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+        <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+      </CheckboxGroup>
     </VStack>
+  ),
 };
 
 export const Errored: Story = {
-    render: () =>
-        <CheckboxGroup
-            id="errored"
-            invalidText="Error message for the full group."
-            isInvalid
-            labelText="Errored Checkbox Group"
-            name="errored-example"
-        >
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
+  render: () => (
+    <CheckboxGroup
+      id="errored"
+      invalidText="Error message for the full group."
+      isInvalid
+      labelText="Errored Checkbox Group"
+      name="errored-example"
+    >
+      <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+      <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+      <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+      <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+    </CheckboxGroup>
+  ),
 };
 
 export const Required: Story = {
-    render: () =>
-        <CheckboxGroup
-            id="required"
-            isRequired
-            labelText="Required Checkbox Group"
-            name="required-example"
-            helperText="The reason for being required."
-        >
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
+  render: () => (
+    <CheckboxGroup
+      id="required"
+      isRequired
+      labelText="Required Checkbox Group"
+      name="required-example"
+      helperText="The reason for being required."
+    >
+      <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+      <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+      <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+      <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+    </CheckboxGroup>
+  ),
 };
 
 export const Disabled: Story = {
-    render: () =>
-        <CheckboxGroup
-            id="disabled"
-            isDisabled
-            labelText="Disabled Checkbox Group"
-            name="disabled-example"
-            helperText="The reason for being disabled."
-        >
-            <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
-            <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
-            <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
-            <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
-        </CheckboxGroup>
+  render: () => (
+    <CheckboxGroup
+      id="disabled"
+      isDisabled
+      labelText="Disabled Checkbox Group"
+      name="disabled-example"
+      helperText="The reason for being disabled."
+    >
+      <Checkbox id="checkbox-2" value="2" labelText="Checkbox 2" />
+      <Checkbox id="checkbox-3" value="3" labelText="Checkbox 3" />
+      <Checkbox id="checkbox-4" value="4" labelText="Checkbox 4" />
+      <Checkbox id="checkbox-5" value="5" labelText="Checkbox 5" />
+    </CheckboxGroup>
+  ),
 };
 
 export const WithJSXElementLabels: Story = {
-    render: () =>
-        <CheckboxGroup
-            id="full-width"
-            isFullWidth
-            labelText="Checkbox Group"
-            name="checkbox-example"
-        >
-            <Checkbox
-                labelText={
-                    <Flex>
-                        <span>Arts</span>
-                        <Spacer />
-                        <span>4</span>
-                    </Flex>
-                }
-                value="Arts"
-                id="arts"
-            />
-            <Checkbox
-                labelText={
-                    <Flex>
-                        <span>English</span>
-                        <Spacer />
-                        <span>23</span>
-                    </Flex>
-                }
-                value="English"
-                id="english"
-            />
-            <Checkbox
-                labelText={
-                    <Flex>
-                        <span>Science</span>
-                        <Spacer />
-                        <span>10</span>
-                    </Flex>
-                }
-                value="Science"
-                id="science"
-            />
-            <Checkbox
-                labelText={
-                    <Flex>
-                        <span>Math</span>
-                        <Spacer />
-                        <span>3</span>
-                    </Flex>
-                }
-                value="Math"
-                id="math"
-            />
-        </CheckboxGroup>
+  render: () => (
+    <CheckboxGroup
+      id="full-width"
+      isFullWidth
+      labelText="Checkbox Group"
+      name="checkbox-example"
+    >
+      <Checkbox
+        labelText={
+          <Flex>
+            <span>Arts</span>
+            <Spacer />
+            <span>4</span>
+          </Flex>
+        }
+        value="Arts"
+        id="arts"
+      />
+      <Checkbox
+        labelText={
+          <Flex>
+            <span>English</span>
+            <Spacer />
+            <span>23</span>
+          </Flex>
+        }
+        value="English"
+        id="english"
+      />
+      <Checkbox
+        labelText={
+          <Flex>
+            <span>Science</span>
+            <Spacer />
+            <span>10</span>
+          </Flex>
+        }
+        value="Science"
+        id="science"
+      />
+      <Checkbox
+        labelText={
+          <Flex>
+            <span>Math</span>
+            <Spacer />
+            <span>3</span>
+          </Flex>
+        }
+        value="Math"
+        id="math"
+      />
+    </CheckboxGroup>
+  ),
 };
 
 // Set up CheckboxGoup with indeterminate state option using React.useState
 function IndeterminateCheckboxExample() {
-    const [checkedItems, setCheckedItems] = React.useState([false, false]);
-    const allChecked = checkedItems.every(Boolean);
-    const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
-    return <CheckboxGroup
-        id="indeterminate"
-        labelText="Indeterminate Example"
-        name="indeterminate-example"
+  const [checkedItems, setCheckedItems] = React.useState([false, false]);
+  const allChecked = checkedItems.every(Boolean);
+  const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+  return (
+    <CheckboxGroup
+      id="indeterminate"
+      labelText="Indeterminate Example"
+      name="indeterminate-example"
     >
-        <Checkbox
-            isChecked={allChecked}
-            isIndeterminate={isIndeterminate}
-            onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
-            labelText="Parent Checkbox"
-            id="parent-checkbox"
-        />
-        <Checkbox
-            isChecked={checkedItems[0]}
-            onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
-            labelText="Child Checkbox 1"
-            id="child-checkbox-1"
-        />
-        <Checkbox
-            isChecked={checkedItems[1]}
-            onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
-            labelText="Child Checkbox 2"
-            id="child-checkbox-2"
-        />
-    </CheckboxGroup>;
+      <Checkbox
+        isChecked={allChecked}
+        isIndeterminate={isIndeterminate}
+        onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+        labelText="Parent Checkbox"
+        id="parent-checkbox"
+      />
+      <Checkbox
+        isChecked={checkedItems[0]}
+        onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+        labelText="Child Checkbox 1"
+        id="child-checkbox-1"
+      />
+      <Checkbox
+        isChecked={checkedItems[1]}
+        onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+        labelText="Child Checkbox 2"
+        id="child-checkbox-2"
+      />
+    </CheckboxGroup>
+  );
 }
-export const IndeterminateExample: Story = { render: () => <IndeterminateCheckboxExample /> };
+export const IndeterminateExample: Story = {
+  render: () => <IndeterminateCheckboxExample />,
+};
 
 // Set up CheckboxGoup with progremmatically set selections using React.useState
 function CheckboxGroupValuesUpdateExample() {
-    const [value, setValue] = React.useState([]);
-    const onClick1 = () => {
-        setValue(["art", "science", "math"]);
-    };
-    const onClick2 = () => {
-        setValue(["math", "music", "magic"]);
-    };
-    const onClick3 = () => {
-        setValue(["physics", "science", "chemistry"]);
-    };
-    const onChange = (data) => {
-        setValue(data);
-        console.log("Selected values:", data);
-    };
-    return (
-        <VStack align="stretch" spacing="m">
-            <span>Set values to:</span>
-            <ButtonGroup>
-                <Button id="btn1" onClick={onClick1}>
-                    "art", "science", "math"
-                </Button>
-                <Button id="btn2" onClick={onClick2}>
-                    "math", "music", "magic"
-                </Button>
-                <Button id="btn3" onClick={onClick3}>
-                    "physics", "science", "chemistry"
-                </Button>
-            </ButtonGroup>
-            <CheckboxGroup
-                id="programmatically-update-example"
-                labelText="Course Selection"
-                name="courseSelection"
-                onChange={onChange}
-                value={value}
-            >
-                <Checkbox id="art" labelText="Art" value="art" />
-                <Checkbox id="chemistry" labelText="Chemistry" value="chemistry" />
-                <Checkbox id="english" labelText="English" value="english" />
-                <Checkbox id="magic" labelText="Magic" value="magic" />
-                <Checkbox id="math" labelText="Math" value="math" />
-                <Checkbox id="music" labelText="Music" value="music" />
-                <Checkbox id="physics" labelText="Physics" value="physics" />
-                <Checkbox id="science" labelText="Science" value="science" />
-            </CheckboxGroup>
-        </VStack>
-    );
+  const [value, setValue] = React.useState([]);
+  const onClick1 = () => {
+    setValue(["art", "science", "math"]);
+  };
+  const onClick2 = () => {
+    setValue(["math", "music", "magic"]);
+  };
+  const onClick3 = () => {
+    setValue(["physics", "science", "chemistry"]);
+  };
+  const onChange = (data) => {
+    setValue(data);
+    console.log("Selected values:", data);
+  };
+  return (
+    <VStack align="stretch" spacing="m">
+      <span>Set values to:</span>
+      <ButtonGroup>
+        <Button id="btn1" onClick={onClick1}>
+          "art", "science", "math"
+        </Button>
+        <Button id="btn2" onClick={onClick2}>
+          "math", "music", "magic"
+        </Button>
+        <Button id="btn3" onClick={onClick3}>
+          "physics", "science", "chemistry"
+        </Button>
+      </ButtonGroup>
+      <CheckboxGroup
+        id="programmatically-update-example"
+        labelText="Course Selection"
+        name="courseSelection"
+        onChange={onChange}
+        value={value}
+      >
+        <Checkbox id="art" labelText="Art" value="art" />
+        <Checkbox id="chemistry" labelText="Chemistry" value="chemistry" />
+        <Checkbox id="english" labelText="English" value="english" />
+        <Checkbox id="magic" labelText="Magic" value="magic" />
+        <Checkbox id="math" labelText="Math" value="math" />
+        <Checkbox id="music" labelText="Music" value="music" />
+        <Checkbox id="physics" labelText="Physics" value="physics" />
+        <Checkbox id="science" labelText="Science" value="science" />
+      </CheckboxGroup>
+    </VStack>
+  );
 }
-export const ProgrammaticallyUpdateValues: Story = { render: () => <CheckboxGroupValuesUpdateExample /> };
+export const ProgrammaticallyUpdateValues: Story = {
+  render: () => <CheckboxGroupValuesUpdateExample />,
+};
