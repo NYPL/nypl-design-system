8396154b1675afbfde4cb6bc9fb614a5fcfbe607
Edwin Guzman <edwin.gzmn@gmail.com>
2023-05-22 14:52:00 -0400

    Updating Storybook documentation for List and StyledList components

---
+++ src/components/StyledList/StyledList.stories.tsx
@@ -0,0 +1,130 @@
+import { Box, VStack } from "@chakra-ui/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+
+import Heading from "../Heading/Heading";
+import StyledList from "./StyledList";
+
+const meta: Meta<typeof StyledList> = {
+  title: "Components/Typography & Styles/StyledList",
+  component: StyledList,
+  decorators: [withDesign],
+  argTypes: {
+    children: { table: { disable: true } },
+    className: { control: false },
+    id: { control: false },
+    listItems: { control: false },
+    style: { control: false },
+    textSize: { table: { defaultValue: { summary: "default" } } },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof StyledList>;
+
+const itemGroups = [
+  'FULL TITLE: "...and for the City of New York"',
+  'ALTERNATIVE TITLES: "...Society of New York delivered at..." + 1 more',
+  'SUBJECTS: "...City of New York, in the" + 2 more',
+];
+const listItems = [
+  <>
+    FULL TITLE:{" "}
+    <i>
+      "...and for the <b>City of New York</b>"
+    </i>
+  </>,
+  <>
+    ALTERNATIVE TITLES:{" "}
+    <i>
+      "...Society of <b>New York</b> delivered at..."
+    </i>{" "}
+    + 1 more
+  </>,
+  <>
+    SUBJECTS:{" "}
+    <i>
+      "...<b>City of New York</b>, in the"
+    </i>{" "}
+    + 2 more
+  </>,
+];
+
+/**
+ * Main Story for the StyledList component. This must contains the `args`
+ * and `parameters` properties in this object.
+ */
+export const WithControls: Story = {
+  args: {
+    className: undefined,
+    id: "nypl-styledlist",
+    listItems: itemGroups,
+    style: "capped",
+    textSize: "default",
+  },
+  render: (args) => <StyledList {...args} />,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/CRPxcR5Olmz50IrFZxvLKE/DRB---Search-Highlighting-(SFR-1519)?node-id=1223%3A11775",
+    },
+    jest: ["StyledList.test.tsx"],
+  },
+};
+
+// The following are additional StyledList example Stories.
+export const LiChildren: Story = {
+  render: () => (
+    <StyledList>
+      <li>
+        FULL TITLE:{" "}
+        <i>
+          "...and for the <b>City of New York</b>"
+        </i>
+      </li>
+      <li>
+        ALTERNATIVE TITLES:{" "}
+        <i>
+          "...Society of <b>New York</b> delivered at..."
+        </i>{" "}
+        + 1 more
+      </li>
+      <li>
+        SUBJECTS:{" "}
+        <i>
+          "...<b>City of New York</b>, in the"
+        </i>{" "}
+        + 2 more
+      </li>
+    </StyledList>
+  ),
+  name: "li Children",
+};
+
+export const ListItemsProp: Story = {
+  render: () => <StyledList listItems={listItems} />,
+  name: "listItems Prop",
+};
+
+export const FontSize: Story = {
+  render: () => (
+    <VStack align="stretch" spacing="l">
+      <Box>
+        <Heading level="three">Caption</Heading>
+        <StyledList textSize="caption" listItems={listItems} />
+      </Box>
+      <Box>
+        <Heading level="three">Default</Heading>
+        <StyledList textSize="default" listItems={listItems} />
+      </Box>
+      <Box>
+        <Heading level="three">Mini</Heading>
+        <StyledList textSize="mini" listItems={listItems} />
+      </Box>
+      <Box>
+        <Heading level="three">Tag</Heading>
+        <StyledList textSize="tag" listItems={listItems} />
+      </Box>
+    </VStack>
+  ),
+};
