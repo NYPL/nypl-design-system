749f2c8c2f89b6ab30e875a00d3cf97bbdd4ded3
Edwin Guzman <edwin.gzmn@gmail.com>
2023-06-09 16:18:00 -0400

    testing global overrides

--- src/theme/provider.tsx
+++ src/theme/provider.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { ChakraProvider, ChakraProviderProps } from "@chakra-ui/react";
+import { ChakraProvider, ChakraProviderProps } from "./internalProvider";
 import theme from "./index";
 
 const DSProvider = ({
   children,
   colorModeManager,
+  resetCSS = true,
+  disableGlobalStyle = false,
 }: React.PropsWithChildren<ChakraProviderProps>) => (
-  <ChakraProvider colorModeManager={colorModeManager} theme={theme}>
+  <ChakraProvider
+    colorModeManager={colorModeManager}
+    theme={theme}
+    resetCSS={resetCSS}
+    disableGlobalStyle={disableGlobalStyle}
+  >
     {children}
   </ChakraProvider>
 );
