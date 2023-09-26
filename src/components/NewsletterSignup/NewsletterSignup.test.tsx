5983419b347127880013e5f4645ede124a72b687
Andrew Arnold <andrewarnold@nypl.org>
2023-09-26 16:07:49 -0400

    NewsletterSignup Tests Round 1

--- src/components/NewsletterSignup/NewsletterSignup.test.tsx
+++ src/components/NewsletterSignup/NewsletterSignup.test.tsx
@@ -1,334 +1,297 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+//import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 import * as React from "react";
 import renderer from "react-test-renderer";
 
 import NewsletterSignup from "./NewsletterSignup";
+import { sectionDataMap } from "../../helpers/types";
 
 describe("NewsletterSignup Accessibility", () => {
-  it("passes axe accessibility when closed", async () => {
-    const onSubmit = jest.fn();
+  const onSubmit = jest.fn();
+  it("Form state passes accessibility", async () => {
     const { container } = render(
-      <NewsletterSignup title="Help and Feeback" onSubmit={onSubmit} />
+      <NewsletterSignup
+        title="Testing"
+        onSubmit={onSubmit}
+        formHelperText={"Form helper."}
+      />
     );
     expect(await axe(container)).toHaveNoViolations();
   });
 
-  it("passes axe accessibility when opened", async () => {
-    const onSubmit = jest.fn();
+  it("Error state passes accessibility", async () => {
     const { container } = render(
-      <NewsletterSignup title="Help and Feeback" onSubmit={onSubmit} />
+      <NewsletterSignup title="Testing" onSubmit={onSubmit} view={"error"} />
     );
+    expect(await axe(container)).toHaveNoViolations();
+  });
 
-    expect(screen.queryByText(/Comment/i)).not.toBeInTheDocument();
+  it("Confirmation state passes accessibility", async () => {
+    const { container } = render(
+      <NewsletterSignup
+        title="Testing"
+        onSubmit={onSubmit}
+        view={"confirmation"}
+      />
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  });
 
-    screen.getByText("Help and Feeback").click();
-    // Just to make sure the dialog is opened.
-    expect(screen.getByText(/Comment/i)).toBeInTheDocument();
+  it("Bad email state passes accessibility", async () => {
+    const { container } = render(
+      <NewsletterSignup
+        title="Testing"
+        onSubmit={onSubmit}
+        view={"form"}
+        isInvalidEmail={true}
+        formHelperText={"That is not allowed."}
+      />
+    );
     expect(await axe(container)).toHaveNoViolations();
   });
 });
 
-describe("NewsletterSignup", () => {
+describe("NewsletterSignup Unit Tests", () => {
+  // If you want to see what's happening, insert below render()
+  // screen.debug();
   let onSubmit = jest.fn();
-
-  it("renders a button component", () => {
-    render(<NewsletterSignup title="Help and Feedback" onSubmit={onSubmit} />);
-
-    expect(
-      screen.getByRole("button", { name: "Help and Feedback" })
-    ).toBeInTheDocument();
-  });
-
-  it("renders the basic content when opened", () => {
-    render(<NewsletterSignup title="Help and Feedback" onSubmit={onSubmit} />);
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(screen.getByTestId("title")).toHaveTextContent("Help and Feedback");
-    expect(
-      screen.getByRole("textbox", { name: /comment/i })
-    ).toBeInTheDocument();
-    expect(screen.getByText(/500 characters remaining/i)).toBeInTheDocument();
-    expect(screen.getByRole("link")).toHaveTextContent("Privacy Policy");
-    // Does not render the radio group or email fields:
-    expect(
-      screen.queryByText(/What is your feedback about/i)
-    ).not.toBeInTheDocument();
-    expect(screen.queryByLabelText(/email/i)).not.toBeInTheDocument();
-
-    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "Cancel" })).toBeInTheDocument();
-  });
-
-  it("renders optional radio group and email field", () => {
-    render(
-      <NewsletterSignup
-        onSubmit={onSubmit}
-        showCategoryField
-        showEmailField
-        title="Help and Feedback"
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(
-      screen.getByText(/what is your feedback about/i)
-    ).toBeInTheDocument();
-    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
-  });
-
-  it("sets the invalid state for the comment and email field", () => {
-    render(
-      <NewsletterSignup
-        isInvalidComment
-        isInvalidEmail
-        onSubmit={onSubmit}
-        showCategoryField
-        showEmailField
-        title="Help and Feedback"
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(screen.getByText(/please fill out this field/i)).toBeInTheDocument();
-    expect(
-      screen.getByText(/please enter a valid email address/i)
-    ).toBeInTheDocument();
-  });
-
-  it("renders optional additional description text", () => {
-    render(
-      <NewsletterSignup
-        descriptionText="Please share your question or feedback."
-        title="Help and Feedback"
-        onSubmit={onSubmit}
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(
-      screen.getByText(/Please share your question or feedback/i)
-    ).toBeInTheDocument();
-  });
-
-  it("renders optional notification text or JSX", () => {
-    const { rerender } = render(
-      <NewsletterSignup
-        notificationText="Call Number: JFE 95-8555"
-        title="Help and Feedback"
-        onSubmit={onSubmit}
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(screen.getByText(/Call Number: JFE 95-8555/i)).toBeInTheDocument();
-
-    rerender(
-      <NewsletterSignup
-        notificationText={<p data-testid="paragraph">JSX notification</p>}
-        title="Help and Feedback"
-        onSubmit={onSubmit}
-      />
-    );
-
-    expect(screen.getByTestId("paragraph")).toHaveTextContent(
-      /jsx notification/i
-    );
-  });
-
-  it("renders the `confirmation` screen through the `view` prop", () => {
-    render(
-      <NewsletterSignup
-        title="Help and Feedback"
-        onSubmit={onSubmit}
-        view="confirmation"
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(
-      screen.getByText(/thank you for submitting your feedback/i)
-    ).toBeInTheDocument();
-    expect(
-      screen.queryByText(
-        /if you provided an email address and require a response/i
-      )
-    ).not.toBeInTheDocument();
-  });
-
-  it("renders the email `confirmation` message when showEmailField is true", () => {
-    render(
-      <NewsletterSignup
-        onSubmit={onSubmit}
-        showEmailField
-        title="Help and Feedback"
-        view="confirmation"
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(
-      screen.getByText(
-        /if you provided an email address and require a response/i
-      )
-    ).toBeInTheDocument();
-  });
-
-  it("renders the `error` screen through the `view` prop", () => {
-    render(
-      <NewsletterSignup
-        title="Help and Feedback"
-        onSubmit={onSubmit}
-        view="error"
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    expect(screen.getByText(/oops! something went wrong/i)).toBeInTheDocument();
-  });
-
-  it("submits the form and returns the submitted data", () => {
-    let submittedValues;
-    let onSubmit = (values) => {
-      submittedValues = values;
-    };
-    render(
-      <NewsletterSignup
-        title="Help and Feedback"
-        showCategoryField
-        showEmailField
-        onSubmit={onSubmit}
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    // The first comment field is the radio button.
-    const commentField = screen.getAllByLabelText(/comment/i)[1];
-    const emailField = screen.getByLabelText(/email/i);
-    const submit = screen.getByRole("button", { name: "Submit" });
-
-    screen.getByText(/bug/i).click();
-    userEvent.type(commentField, "This is a comment");
-    userEvent.type(emailField, "email@email.com");
-
-    submit.click();
-
-    expect(submittedValues).toEqual({
-      category: "bug",
-      comment: "This is a comment",
-      email: "email@email.com",
+  describe("Renders the Minimum Required Elements", () => {
+    it("Renders the form", () => {
+      render(<NewsletterSignup title="Testing" onSubmit={onSubmit} />);
+      expect(screen.getByRole("form")).toBeInTheDocument();
     });
-  });
-
-  it("adds hidden fields data to the submitted data", () => {
-    const hiddenFields = {
-      "hidden-field-1": "hidden-field-value-1",
-      "hidden-field-2": "hidden-field-value-2",
-    };
-    let submittedValues;
-    let onSubmit = (values) => {
-      submittedValues = values;
-    };
-    render(
-      <NewsletterSignup
-        hiddenFields={hiddenFields}
-        title="Help and Feedback"
-        showCategoryField
-        showEmailField
-        onSubmit={onSubmit}
-      />
-    );
-
-    const button = screen.getByRole("button", { name: "Help and Feedback" });
-
-    button.click();
-
-    // The first comment field is the radio button.
-    const commentField = screen.getAllByLabelText(/comment/i)[1];
-    const emailField = screen.getByLabelText(/email/i);
-    const submit = screen.getByRole("button", { name: "Submit" });
-
-    screen.getByText(/bug/i).click();
-    userEvent.type(commentField, "This is a comment");
-    userEvent.type(emailField, "email@email.com");
-
-    submit.click();
-
-    expect(submittedValues).toEqual({
-      category: "bug",
-      comment: "This is a comment",
-      email: "email@email.com",
-      "hidden-field-1": "hidden-field-value-1",
-      "hidden-field-2": "hidden-field-value-2",
+    it("Renders the input textbox", () => {
+      render(<NewsletterSignup title="Testing" onSubmit={onSubmit} />);
+      expect(screen.getByRole("textbox")).toBeInTheDocument();
     });
-  });
+    it("Renders the button", () => {
+      render(<NewsletterSignup title="Testing" onSubmit={onSubmit} />);
+      expect(
+        screen.getByRole("button", { name: "Submit" })
+      ).toBeInTheDocument();
+    });
+    it("Renders the title", () => {
+      render(<NewsletterSignup title="Testing" onSubmit={onSubmit} />);
+      expect(screen.getByText(/Testing/i)).toBeInTheDocument();
+    });
+  }); // Close minimum elements tests
 
-  it("transitions to the `form` screen from the `error` screen", () => {
-    render(
+  describe("Renders the Optional descriptionText and formHelperText Values", () => {
+    // Note: newsletterSignupType tests are covered in the snapshot tests below.
+    const testNewsletterSignup = (
       <NewsletterSignup
-        title="Help and Feedback"
+        descriptionText="Do not send cash."
+        formHelperText={"Just trying to help"}
+        title="Testing"
         onSubmit={onSubmit}
-        view="error"
       />
     );
+    it("Renders the description", () => {
+      render(testNewsletterSignup);
+      expect(screen.getByText(/Do not send cash./i)).toBeInTheDocument();
+    });
 
-    // Open the dialog.
-    screen.queryByRole("button", { name: "Help and Feedback" }).click();
+    it("Renders the helper text", () => {
+      render(testNewsletterSignup);
+      expect(screen.getByText(/Just trying to help/i)).toBeInTheDocument();
+    });
+  }); // Close optional text elements test
 
-    const button = screen.queryByRole("button", { name: "Try Again" });
-    expect(
-      screen.queryByText(/oops! something went wrong/i)
-    ).toBeInTheDocument();
+  describe("Renders the Feedback Views", () => {
+    it("Renders the error view", () => {
+      render(
+        <NewsletterSignup view={"error"} title="Testing" onSubmit={onSubmit} />
+      );
+      expect(
+        screen.getByText(/Oops! Something went wrong./i)
+      ).toBeInTheDocument();
+      expect(screen.getByTitle("errorFilled icon")).toBeInTheDocument();
+    });
 
-    button.click();
+    it("Renders the confirmation view", () => {
+      render(
+        <NewsletterSignup
+          view={"confirmation"}
+          title="Testing"
+          onSubmit={onSubmit}
+          confirmationText={"Fantastic!"}
+        />
+      );
+      expect(screen.getByText(/Fantastic/i)).toBeInTheDocument();
+      expect(
+        screen.getByTitle("actionCheckCircleFilled icon")
+      ).toBeInTheDocument();
+    });
 
-    // The `error` screen should no longer display.
-    expect(
-      screen.queryByText(/oops! something went wrong/i)
-    ).not.toBeInTheDocument();
-    expect(button).not.toBeInTheDocument();
+    it("Renders the bad email view", () => {
+      render(
+        <NewsletterSignup
+          view={"form"}
+          isInvalidEmail={true}
+          title="Testing"
+          onSubmit={onSubmit}
+        />
+      );
+      expect(
+        screen.getByText(/Please enter a valid email address./i)
+      ).toBeInTheDocument();
+      expect(screen.getByRole("form")).toBeInTheDocument();
+      expect(screen.getByRole("textbox")).toBeInTheDocument();
+      expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+  }); // Close feedback tests
 
-    // We are back at the `form` screen.
-    expect(
-      screen.getByRole("textbox", { name: /comment/i })
-    ).toBeInTheDocument();
-    expect(screen.getByText(/500 characters remaining/i)).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
-  });
+  // @todo These will likely need to be rewritten under the new specs
+  // describe("Behaviors on Button Click", () => {
+  //   it("Disables the button when clicked", () => {
+  //     render(<NewsletterSignup title="Testing" onSubmit={onSubmit} />);
+  //     //screen.debug();
+  //     const button = screen.getByRole("button", { name: "Submit" });
+  //     expect(button).not.toHaveAttribute("disabled");
+  //     button.click();
+  //     //screen.debug();
+  //     expect(button).toHaveAttribute("");
+  //   });
+  //
+  //   it("Adds hidden fields data to the submitted data", () => {
+  //     const hiddenFields = {
+  //       "hidden-field-1": "hidden-field-value-1",
+  //       "hidden-field-2": "hidden-field-value-2",
+  //     };
+  //     let submittedValues;
+  //     let onSubmit = (values) => {
+  //       submittedValues = values;
+  //     };
+  //     render(
+  //       <NewsletterSignup
+  //         hiddenFields={hiddenFields}
+  //         title="Testing"
+  //         onSubmit={onSubmit}
+  //       />
+  //     );
+  //
+  //     const emailField = screen.getByLabelText(/email/i);
+  //     userEvent.type(emailField, "email@email.com");
+  //
+  //     const button = screen.getByRole("button", { name: "Submit" });
+  //
+  //     button.click();
+  //
+  //     expect(submittedValues).toEqual({
+  //       email: "email@email.com",
+  //       "hidden-field-1": "hidden-field-value-1",
+  //       "hidden-field-2": "hidden-field-value-2",
+  //     });
+  //   });
+  // }); // Close behaviors on button click
+}); // Close unit tests.
 
-  it("renders the UI snapshot correctly", () => {
-    const basic = renderer
-      .create(<NewsletterSignup title="Help and Feeback" onSubmit={onSubmit} />)
+describe("NewsletterSignup Snapshots", () => {
+  const onSubmit = jest.fn();
+  it("Renders the default form UI snapshot correctly", () => {
+    const view = renderer
+      .create(<NewsletterSignup title="Testing" onSubmit={onSubmit} />)
       .toJSON();
-
-    expect(basic).toMatchSnapshot();
+    expect(view).toMatchSnapshot();
   });
-});
+
+  it("Renders the form UI with formHelperText snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup
+          title="Testing"
+          onSubmit={onSubmit}
+          formHelperText={"You need help."}
+        />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  it("Renders the form UI with description snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup
+          title="Testing"
+          onSubmit={onSubmit}
+          descriptionText={"Why not?"}
+        />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  it("Renders the bad email UI snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup
+          title="Testing"
+          onSubmit={onSubmit}
+          isInvalidEmail={true}
+        />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  it("Renders the error UI snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup title="Testing" onSubmit={onSubmit} view={"error"} />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  it("Renders the default confirmation UI snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup
+          title="Testing"
+          onSubmit={onSubmit}
+          view={"confirmation"}
+        />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  it("Renders the confirmation UI with confirmationText snapshot correctly", () => {
+    const view = renderer
+      .create(
+        <NewsletterSignup
+          title="Testing"
+          onSubmit={onSubmit}
+          view={"confirmation"}
+          confirmationText={"You did great!"}
+        />
+      )
+      .toJSON();
+    expect(view).toMatchSnapshot();
+  });
+
+  describe("Renders each color for each newsletterSignupType correctly", () => {
+    // newsletterSignupType values are determined by the types contained in the sectionDataMap. So it is safe to use the map directly.
+    sectionDataMap.map((section) => {
+      it(
+        "Renders " +
+          section.type +
+          " color band with " +
+          section.colorVals.primary,
+        () => {
+          const view = renderer
+            .create(
+              <NewsletterSignup
+                title="Testing"
+                onSubmit={onSubmit}
+                newsletterSignupType={section.type}
+              />
+            )
+            .toJSON();
+          expect(view).toMatchSnapshot();
+        }
+      );
+    });
+  }); // Close colors snapshots
+}); // Close snapshots
