e3a689ab9228cb49fbcf2199aacf3d7e4903ab2f
bigfishdesign13 <marty@brutvan.com>
2023-09-27 13:59:07 -0400

    archiving docs for v1.7.3

--- .storybook/ReservoirTheme.js
+++ .storybook/ReservoirTheme.js
@@ -3,6 +3,6 @@ import logo from "./public/nypl-reservoir-design-system-logo.svg";
 
 export default create({
   base: "light",
-  brandTitle: "NYPL Reservoir Design System React Library",
+  brandTitle: "NYPL Reservoir Design System React Library - v1.7.3",
   brandImage: logo,
 });
