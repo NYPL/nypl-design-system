793e937506704e668e8cf022c5891bc662cee02f
Edwin Guzman <edwin.gzmn@gmail.com>
2023-11-16 12:45:04 -0500

    Adding and passing down isLazy from Card to its Image component

---
+++ src/components/Card/cardChangelogData.ts
@@ -0,0 +1,21 @@
+/** This data is used to populate the ComponentChangelogTable component.
+ *
+ * date: string (when adding new entry during development, set value as "Prerelease")
+ * version: string (when adding new entry during development, set value as "Prerelease")
+ * type: "Bug Fix" | "New Feature" | "Update";
+ * affects: array["Accessibility" | "Documentation" | "Functionality" | "Styles"];
+ * notes: array (will render as a bulleted list, add one array element for each list element)
+ */
+import { ChangelogData } from "../../utils/ComponentChangelogTable";
+
+export const changelogData: ChangelogData[] = [
+  {
+    date: "Prerelease",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Functionality", "Documentation"],
+    notes: [
+      "Propagates the `imageProps.isLazy` property down to the `Card`'s internal `Image` component implementation.",
+    ],
+  },
+];
