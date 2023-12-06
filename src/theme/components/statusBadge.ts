4c0a17487dd3bbb22a563f034273ff4ab3634028
Isa Stettler <stettlerisabelle@gmail.com>
2023-12-06 09:49:37 -0500

    update StatusBadge type and theme

--- src/theme/components/statusBadge.ts
+++ src/theme/components/statusBadge.ts
@@ -1,38 +1,46 @@
-const StatusBadge = {
-  baseStyle: {
-    borderRadius: "base",
-    color: "ui.typography.heading",
-    display: "block",
-    fontSize: "desktop.body.body2", // slightly smaller than the default size
-    fontStyle: "italic",
-    py: "inset.extranarrow",
-    px: "inset.narrow",
-    textAlign: "center",
-    width: "fit-content",
-    _dark: {
-      backgroundColor: "dark.ui.bg.active",
-      borderLeft: "4px solid",
-      borderColor: "dark.ui.border.default",
-      color: "dark.ui.typography.heading",
-    },
+import { defineStyleConfig } from "@chakra-ui/react";
+import { defineStyle } from "@chakra-ui/system";
+
+const baseStyle = defineStyle({
+  borderRadius: "base",
+  color: "ui.typography.heading",
+  display: "block",
+  fontSize: "desktop.body.body2", // slightly smaller than the default size
+  fontStyle: "italic",
+  py: "inset.extranarrow",
+  px: "inset.narrow",
+  textAlign: "center",
+  width: "fit-content",
+  _dark: {
+    backgroundColor: "dark.ui.bg.active",
+    borderLeft: "4px solid",
+    borderColor: "dark.ui.border.default",
+    color: "dark.ui.typography.heading",
   },
+});
+
+// Variants
+const low = defineStyle({ bg: "ui.gray.light-cool" });
+const medium = defineStyle({
+  bg: "ui.status.primary",
+  _dark: {
+    borderColor: "ui.status.primary",
+  },
+});
+const high = defineStyle({
+  bg: "ui.status.secondary",
+  _dark: {
+    borderColor: "dark.ui.error.primary",
+  },
+});
+
+const StatusBadge = defineStyleConfig({
+  baseStyle,
   variants: {
-    low: {
-      bg: "ui.gray.light-cool",
-    },
-    medium: {
-      bg: "ui.status.primary",
-      _dark: {
-        borderColor: "ui.status.primary",
-      },
-    },
-    high: {
-      bg: "ui.status.secondary",
-      _dark: {
-        borderColor: "dark.ui.error.primary",
-      },
-    },
+    low,
+    medium,
+    high,
   },
-};
+});
 
 export default StatusBadge;
