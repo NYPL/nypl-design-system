5d89a7855a02ede2acd9a2cfb10d0450d99bf419
Jackie Quach <jackiequach14@gmail.com>
2024-01-04 10:07:45 -0500

    reordrer notification changelog

--- src/components/Notification/notificationChangelogData.ts
+++ src/components/Notification/notificationChangelogData.ts
@@ -10,6 +10,13 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
+    date: "Prerelease",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Styles"],
+    notes: ["Chakra 2.8 update."],
+  },
+  {
     date: "2023-12-07",
     version: "2.1.3",
     type: "Update",
@@ -17,12 +24,5 @@ export const changelogData: ChangelogData[] = [
     notes: [
       "Updated the `notificationHeading` prop to allow JSX to render custom heading elements for accessible heading hierarchy.",
     ],
-  },
-  {
-    date: "Prerelease",
-    version: "Prerelease",
-    type: "Update",
-    affects: ["Styles"],
-    notes: ["Chakra 2.8 update."],
   },
 ];
