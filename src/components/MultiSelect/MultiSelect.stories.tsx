e52ad3d07cf1e77d3d53b7bb7e0d7f79cd308653
Edwin Guzman <edwin.gzmn@gmail.com>
2024-01-26 11:07:55 -0500

    fixing type issue

--- src/components/MultiSelect/MultiSelect.stories.tsx
+++ src/components/MultiSelect/MultiSelect.stories.tsx
@@ -6,6 +6,7 @@ import Heading from "../Heading/Heading";
 import MultiSelect, {
   multiSelectWidthsArray,
   multiSelectListOverflowArray,
+  MultiSelectProps,
 } from "./MultiSelect";
 import Text from "../Text/Text";
 import useMultiSelect from "../../hooks/useMultiSelect";
@@ -501,7 +502,7 @@ const MultiSelectStory = ({
   items,
   listOverflow = "scroll",
   width = "full",
-}) => {
+}: Partial<MultiSelectProps>) => {
   // Example with custom hook useMultiSelect.
   const { onChange, onMixedStateChange, onClear, selectedItems } =
     useMultiSelect();
