5091e264e412fda15c0a140d3cceccaeb6b5d262
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-01-29 13:16:21 -0500

    Accordion box sizing

--- src/theme/components/accordion.ts
+++ src/theme/components/accordion.ts
@@ -13,6 +13,11 @@ const containerStyles = {
 };
 const buttonStyles = {
   borderWidth: "1px",
+  " > span ": {
+    display: "flex",
+    alignItems: "center",
+    minHeight: "24px",
+  },
   color: "ui.typography.heading",
   fontWeight: "medium",
   _dark: {
