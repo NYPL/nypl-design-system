450c3be1d5a71924d2b3aa0245f79629382d32a8
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-02-07 16:34:28 -0500

    Aria label!

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -193,6 +193,7 @@ export const Menu = chakra(
       const getButton = (isOpen) => (
         <MenuButton
           sx={styles.menuButton}
+          aria-label={labelText}
           backgroundColor={isOpen ? "ui.link.primary-05 !important" : "unset"}
         >
           {showLabel && (
