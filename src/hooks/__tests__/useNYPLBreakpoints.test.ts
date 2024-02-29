ae5472271371be3eecc53547ae2e959ed7a55d4c
bigfishdesign13 <marty@brutvan.com>
2024-02-29 11:11:37 -0500

    fixing tests

--- src/hooks/__tests__/useNYPLBreakpoints.test.ts
+++ src/hooks/__tests__/useNYPLBreakpoints.test.ts
@@ -20,45 +20,70 @@ describe("useNYPLBreakpoints", () => {
     expect(result.current).toEqual({
       isLargerThanSmall: false,
       isLargerThanMedium: false,
-      isLargerThanMobile: false,
       isLargerThanLarge: false,
       isLargerThanXLarge: false,
+      isLargerThanSmallMobile: false,
+      isLargerThanLargeMobile: false,
+      isLargerThanSmallTablet: false,
+      isLargerThanLargeTablet: false,
+      isLargerThanMobile: false,
+      isLargerThanTablet: false,
     });
 
     act(() => window.resizeTo(480, 600));
     expect(result.current).toEqual({
       isLargerThanSmall: true,
       isLargerThanMedium: false,
-      isLargerThanMobile: false,
       isLargerThanLarge: false,
       isLargerThanXLarge: false,
+      isLargerThanSmallMobile: true,
+      isLargerThanLargeMobile: false,
+      isLargerThanSmallTablet: false,
+      isLargerThanLargeTablet: false,
+      isLargerThanMobile: false,
+      isLargerThanTablet: false,
     });
 
     act(() => window.resizeTo(768, 600));
     expect(result.current).toEqual({
       isLargerThanSmall: true,
       isLargerThanMedium: true,
-      isLargerThanMobile: true,
       isLargerThanLarge: false,
       isLargerThanXLarge: false,
+      isLargerThanSmallMobile: true,
+      isLargerThanLargeMobile: true,
+      isLargerThanSmallTablet: false,
+      isLargerThanLargeTablet: false,
+      isLargerThanMobile: true,
+      isLargerThanTablet: false,
     });
 
     act(() => window.resizeTo(1024, 600));
     expect(result.current).toEqual({
       isLargerThanSmall: true,
       isLargerThanMedium: true,
-      isLargerThanMobile: true,
       isLargerThanLarge: true,
       isLargerThanXLarge: false,
+      isLargerThanSmallMobile: true,
+      isLargerThanLargeMobile: true,
+      isLargerThanSmallTablet: true,
+      isLargerThanLargeTablet: false,
+      isLargerThanMobile: true,
+      isLargerThanTablet: false,
     });
 
     act(() => window.resizeTo(1280, 600));
     expect(result.current).toEqual({
       isLargerThanSmall: true,
       isLargerThanMedium: true,
-      isLargerThanMobile: true,
       isLargerThanLarge: true,
       isLargerThanXLarge: true,
+      isLargerThanSmallMobile: true,
+      isLargerThanLargeMobile: true,
+      isLargerThanSmallTablet: true,
+      isLargerThanLargeTablet: true,
+      isLargerThanMobile: true,
+      isLargerThanTablet: true,
     });
   });
 });
