0f399866667c1e0c34c07c355ce1e9a6a15d40df
Edwin Guzman <edwin.gzmn@gmail.com>
2024-05-14 12:13:14 -0400

    Fixing the desktop styling flicker in ButtonGroup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@ Currently, this repo is in Prerelease. When it is released, this project will ad
 
 ## Prerelease
 
+### Fixes
+
+- Fixes the desktop styling flicker in the `ButtonGroup` component.
+
 ## 3.1.2 (May 9, 2024)
 
 ### Adds
--- src/components/ButtonGroup/buttonGroupChangelogData.ts
+++ src/components/ButtonGroup/buttonGroupChangelogData.ts
@@ -10,6 +10,15 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
+    date: "Prerelease",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Styles"],
+    notes: [
+      "Fixes desktop flicker issue where the mobile view was briefly displayed before the desktop styles were applied.",
+    ],
+  },
+  {
     date: "2024-03-14",
     version: "3.0.0",
     type: "Update",
