22797526e7b07e879657b40646657e4463b52dfd
Diego Cohen <diego.cohen3@gmail.com>
2024-07-30 13:17:42 -0400

    Add entry to changelog data

--- src/components/Banner/bannerChangelogData.ts
+++ src/components/Banner/bannerChangelogData.ts
@@ -10,6 +10,13 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
+    date: "2024-07-30",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Functionality"],
+    notes: ["Render HTML content when passed as a string in the `content` prop."],
+  },
+  {
     date: "2024-07-25",
     version: "3.2.0",
     type: "Update",
