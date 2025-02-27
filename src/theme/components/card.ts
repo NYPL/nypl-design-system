6ee4c458b6454e0242ddf0eec2e79fa539a7b294
Jackie Quach <jackiequach14@gmail.com>
2025-02-27 16:00:01 -0500

    fix data attribute name

--- src/theme/components/card.ts
+++ src/theme/components/card.ts
@@ -169,7 +169,7 @@ const ReservoirCard = defineMultiStyleConfig({
           ...setContainerStyles({
             breakpoint: "md",
             styles: {
-              "[data-wrapper]": {
+              "[data-cardWrapper]": {
                 flexFlow: isRow ? "row" : null,
               },
               "[data-actions]": {
