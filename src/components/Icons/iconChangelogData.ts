93ebc0003317c15bd1c1077cda8fded970e40b49
Edwin Guzman <edwin.gzmn@gmail.com>
2025-04-07 10:09:44 -0400

    changelog cleanup

--- src/components/Icons/iconChangelogData.ts
+++ src/components/Icons/iconChangelogData.ts
@@ -10,6 +10,13 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
+    date: "Prerelease",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Functionality"],
+    notes: ["Removes the depreacted `IconRotationTypes` type."],
+  },
+  {
     date: "2025-02-13",
     version: "3.5.4",
     type: "Update",
--- src/components/ProgressIndicator/progressIndicatorChangelogData.ts
+++ src/components/ProgressIndicator/progressIndicatorChangelogData.ts
@@ -10,6 +10,13 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
+    date: "Prerelease",
+    version: "Prerelease",
+    type: "Update",
+    affects: ["Functionality"],
+    notes: ["Removes deprecated `darkMode` prop."],
+  },
+  {
     date: "2024-10-24",
     version: "3.4.1",
     type: "Update",
