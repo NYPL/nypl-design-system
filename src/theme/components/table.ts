74e83db2d119fa5f6a799f2ab2b7a6c7c5e61bfc
Olivia Wong <oliviawong@nypl.org>
2025-06-09 11:04:37 -0400

    Merges reservoir-v4 and updates based on feedback

--- src/theme/components/table.ts
+++ src/theme/components/table.ts
@@ -56,16 +56,19 @@ export const fixedColumnStyles = (
   },
 });
 
-const tableTextSizes = (textSizeValue: string = "body1") => ({
-  columnHeading:
-    textSizeValue === "body1" ? "desktop.body.body2" : "desktop.caption",
-  tableBody:
-    textSizeValue === "body1"
-      ? "desktop.body.body1"
-      : textSizeValue === "body2"
-      ? "desktop.body.body2"
-      : "desktop.caption",
-});
+const tableTextSizes = (textSizeValue: string = "body1") => {
+  const fontSizeMap = {
+    body1: "desktop.body.body1",
+    body2: "desktop.body.body2",
+    caption: "desktop.caption",
+  };
+
+  return {
+    columnHeading:
+      textSizeValue === "body1" ? "desktop.body.body2" : "desktop.caption",
+    tableBody: fontSizeMap[textSizeValue],
+  };
+};
 
 export const baseTRStyles = (
   columnHeadersBackgroundColor = "",
