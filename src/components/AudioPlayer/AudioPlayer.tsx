f60eaff027fd6a967c0d8d1e810a6778ed736c7f
Diego Cohen <diego.cohen3@gmail.com>
2025-06-12 10:46:02 -0400

    Update audio player changelog

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -157,11 +157,9 @@ export const AudioPlayer: ChakraComponent<
           )}
           {/* Displays a fallback message to users when JavaScript is disabled. */}
           <noscript>
-            <Box 
-              __css={styles.invalid} 
-            >
-              The audio player requires JavaScript to function. 
-              Please enable JavaScript in your browser settings and refresh the page.
+            <Box __css={styles.invalid}>
+              The audio player requires JavaScript to function. Please enable
+              JavaScript in your browser settings and refresh the page.
             </Box>
           </noscript>
         </ComponentWrapper>
