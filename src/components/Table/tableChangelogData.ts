4362865a1e76bfcbbc6ac8aab46f0b6dbf1112e8
bigfishdesign13 <marty@brutvan.com>
2025-06-18 15:03:33 -0400

    updated CHANGELOG

--- src/components/Table/tableChangelogData.ts
+++ src/components/Table/tableChangelogData.ts
@@ -13,9 +13,10 @@ export const changelogData: ChangelogData[] = [
     date: "Prerelease",
     version: "Prerelease",
     type: "Update",
-    affects: ["Styles"],
+    affects: ["Functionality", "Styles"],
     notes: [
       "Changes theme name from `CustomTable` to `ReservoirTable` for consistency.",
+      "Removed the `tableTextSize` prop in favor of using the native `fontSize` CSS prop.",
     ],
   },
   {
