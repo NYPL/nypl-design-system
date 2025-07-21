82095f5b50a0835c66de9901d4680fc7acf2c141
Edwin Guzman <edwin.gzmn@gmail.com>
2025-07-21 16:54:33 -0400

    updates based on feedback

--- src/components/MultiSelect/MultiSelect.tsx
+++ src/components/MultiSelect/MultiSelect.tsx
@@ -56,7 +56,8 @@ export interface MultiSelectProps extends BoxProps {
   listOverflow?: MultiSelectListOverflowTypes;
   /** The action to perform for the clear/reset button of individual MultiSelects. */
   onClear?: () => void;
-  /** The action to perform on the checkbox's onChange function. */
+  /** The action to perform on the checkbox's onChange function. Note, if using
+   * this prop, it must be of the type listed. */
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   /** The action to perform for a mixed state checkbox (parent checkbox). */
   onMixedStateChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
