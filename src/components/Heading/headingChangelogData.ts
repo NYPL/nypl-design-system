22693ddf27f41b036b898a65a29ed7c65dc6c74f
Edwin Guzman <edwin.gzmn@gmail.com>
2025-07-30 16:43:33 -0400

    wrapping up the test of the docs pages to update

--- src/components/Heading/headingChangelogData.ts
+++ src/components/Heading/headingChangelogData.ts
@@ -20,7 +20,7 @@ export const changelogData: ChangelogData[] = [
       "Removes explicit `className` prop as interface can be extended to include Chakra prop or HTML attribute types.",
       "Removes children count warning.",
       'Removes the `noSpace` prop and sets all margins to "0" by default.',
-      "Updated the internal use of ids to reduce `undefined` ids in the DOM.",
+      "Updates the internal use of ids to reduce `undefined` ids in the DOM.",
       "Adds `data-testid` value of `ds-heading` to the parent element.",
     ],
   },
