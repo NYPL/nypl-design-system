65a7ebc438a98acf1b66b02df87ab87c7865489f
Isa Stettler <stettlerisabelle@gmail.com>
2025-11-04 13:53:13 -0500

    remove iframe from global focus settings

--- src/theme/foundations/global.ts
+++ src/theme/foundations/global.ts
@@ -38,7 +38,7 @@ const global = {
       _focus: activeFocus(),
     },
   },
-  "iframe, [href], [tabindex], [contentEditable='true']": {
+  "[href], [tabindex], [contentEditable='true']": {
     _focus: activeFocus(),
   },
   // p: textMargin,
