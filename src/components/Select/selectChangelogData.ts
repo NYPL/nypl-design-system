31bb17ae7a2b40dbc3c4e86a8629b0ca2014fea1
Edwin Guzman <edwin.gzmn@gmail.com>
2026-03-31 13:14:10 -0400

    Prep for release 4.1.6

--- src/components/Select/selectChangelogData.ts
+++ src/components/Select/selectChangelogData.ts
@@ -10,8 +10,8 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
-    date: "Prerelease",
-    version: "Prerelease",
+    date: "2026-03-31",
+    version: "4.1.6",
     type: "Update",
     affects: ["Styles"],
     notes: [
