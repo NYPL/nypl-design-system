089afcb2e8aab66d25e097709d7a93d34b6bfb5f
Edwin Guzman <edwin.gzmn@gmail.com>
2026-07-21 13:45:39 -0400

    Updating ref to compiled css file

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
           "@emotion/react": "Emotion",
         },
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "style.css") return "styles.css";
+          if (assetInfo.name === "design-system-react-components.css") return "styles.css";
           return assetInfo.name || "";
         },
       },
