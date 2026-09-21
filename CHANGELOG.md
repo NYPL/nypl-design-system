f1e81532b51b05ae369fe6b3cf66ddfad3ff0a30
nprotheroNYPL <nicolasprothero@nypl.org>
2026-09-21 13:24:16 -0400

    init changes for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ Currently, this repo is in Prerelease. When it is released, this project will ad
 
 ## Prerelease
 
+## 4.5.1 (September 21, 2026)
+
 ## Updates
 
 - Adds the `isDisabled` prop and associated styling to the `Menu` component's item.
--- src/components/Menu/menuChangelogData.ts
+++ src/components/Menu/menuChangelogData.ts
@@ -10,8 +10,8 @@ import { ChangelogData } from "../../utils/ComponentChangelogTable";
 
 export const changelogData: ChangelogData[] = [
   {
-    date: "Prerelease",
-    version: "Prerelease",
+    date: "2026-09-21",
+    version: "4.5.1",
     type: "Update",
     affects: ["Functionality", "Styles"],
     notes: [
